879c5cbce0ea3ff0236071914cf1ed063dfb18fb
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2008-01-19 05:59:09 +0000

    MglMouseInputBase 継承するように
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@16 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/input/MglMouseInput.cpp
+++ mgllib/src/input/MglMouseInput.cpp
@@ -6,92 +6,12 @@
 //	コンストラクタ
 CMglMouseInput::CMglMouseInput()
 {
-	m_pDevice = NULL;
-	ZeroMemory( m_stateBuf, sizeof(m_stateBuf) );
+
 }
 
 //	デストラクタ
 CMglMouseInput::~CMglMouseInput()
 {
-	Release();
+
 }
 
-//	初期化
-void CMglMouseInput::Init( HWND hWnd, DWORD dwCooperativeFlag )
-{
-	//	基底初期化
-	InitBase();
-
-	//	デフォルトのhWnd取得
-	if ( hWnd == NULL )
-		hWnd = GetDefaultHwnd();
-
-	MyuAssert( m_pDi->CreateDevice( GUID_SysMouse, &m_pDevice, NULL ), DI_OK,
-		"CMglMouseInput::Init  m_pDi->CreateDevice()に失敗" );
-
-	MyuAssert( m_pDevice->SetDataFormat( &c_dfDIMouse ), DI_OK,
-		"CMglMouseInput::Init  m_pDevice->SetDataFormat()に失敗" );
-    
-	MyuAssert( m_pDevice->SetCooperativeLevel( hWnd, dwCooperativeFlag ), DI_OK,
-		"CMglMouseInput::Init  m_pDevice->SetCooperativeLevel()に失敗" );
-
-	Acquire();
-}
-
-//	開放
-void CMglMouseInput::Release()
-{
-	if( m_pDevice )
-        Unacquire();
-    
-    // Release any DirectInput objects.
-    SAFE_RELEASE( m_pDevice );
-}
-
-
-//	デバイスへのアクセス権限を取得
-void CMglMouseInput::Acquire()
-{
-	InitCheck();
-	//	ウインドウフォーカスが外れている時はエラーを返す
-	m_pDevice->Acquire();
-/*
-	MyuAssert( m_pDevice->Acquire(), DI_OK,
-		"CMglMouseInput::Acquire()  m_pDevice->Acquire()に失敗" );
-*/
-}
-
-//	デバイスへのアクセス権限を破棄
-void CMglMouseInput::Unacquire()
-{
-	InitCheck();
-	m_pDevice->Unacquire();
-}
-
-//	ステート情報を取得
-void CMglMouseInput::UpdateStateBuf()
-{
-	InitCheck();
-	ZeroMemory( m_stateBuf, sizeof(m_stateBuf) );
-	if ( m_pDevice->GetDeviceState( sizeof(m_stateBuf), &m_stateBuf ) != DI_OK )
-	{
-		//	失敗したらAcquire()を実行してもう一回試してみる
-		Acquire();
-		m_pDevice->GetDeviceState( sizeof(m_stateBuf), &m_stateBuf );
-
-		/*
-			単にウインドウフォーカスが外れているだけかもしれないので何もしない
-		*/
-
-		//	どうも取得に失敗した時、中身が何か入ってしまっているようだ。
-		ZeroMemory( m_stateBuf, sizeof(m_stateBuf) );
-	}
-}
-
-BOOL CMglMouseInput::IsPressKey( BYTE keyCode )
-{
-	if( PRESSKEY(m_stateBuf[keyCode]) ) 
-		return TRUE;
-	else
-		return FALSE;
-}
--- mgllib/src/input/MglMouseInput.h
+++ mgllib/src/input/MglMouseInput.h
@@ -7,38 +7,19 @@
 #ifndef __MglMouseInput_H__
 #define __MglMouseInput_H__
 
-#include "MglDirectInputBase.h"
+#include "MglMouseInputBase.h"
 
 #define STATEBUF_SIZE	(256)
 
 //	クラス宣言
-class DLL_EXP CMglMouseInput : public CMglDirectInputBase
+class DLL_EXP CMglMouseInput : public CMglMouseInputBase
 {
 protected:
-	_MGL_IDirectInputDevice *m_pDevice;
-	BYTE m_stateBuf[STATEBUF_SIZE];
-
-	void Acquire();
-	void Unacquire();
-
-	void InitCheck()
-	{
-		if ( m_pDevice == NULL )
-			Init();
-	}
 
 public:
 	//	コンストラクタ・デストラクタ
 	CMglMouseInput();
 	virtual ~CMglMouseInput();
-
-	//	初期化と開放
-	void Init( HWND hWnd=NULL, DWORD dwCooperativeFlag=DISCL_NONEXCLUSIVE|DISCL_FOREGROUND );
-	void Release();
-
-	void UpdateStateBuf();
-	BOOL IsPressKey( BYTE keyCode );		//	Is???
-	BOOL IsPressDikey( BYTE keyCode ){ return IsPressKey(keyCode); }
 };
 
 
