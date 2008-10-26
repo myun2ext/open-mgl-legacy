ae6f7033bc2d0d9e350e37efd7dfb4514e0e6dbf
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2008-10-26 14:19:00 +0000

    開発日誌用に不要なコメント取り除きました。
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@214 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib-test/mgl_test/mgl_test.cpp
+++ mgllib-test/mgl_test/mgl_test.cpp
@@ -9,45 +9,14 @@ class CMglTestFrame : public CMglguiWindow
 {
 private:
 	CMglAghImage m_img;
-	CMglAghImage m_img2;
-	CMglBitmapData m_bitmap;
 public:
 	CMglTestFrame(){}
 	void OnInit(){
-		//	2008/10/15
 		EnableEscEnd();
 		m_img.EnableDragMovable();
 		m_img.Load("font_big.tga");
-		m_img2.Load("font_big.tga");
 		CMglguiWindow::RegistControl(&m_img);
-		//CMglguiWindow::RegistControl(&m_img2);
-		/*m_bitmap.Init(&m_img);*/
 	}
-	/*void OnControlLButtonDown(agh::CControlBase* p, int x, int y)
-	{
-		//MessageBox("bbb");
-		p->OnLButtonDown(x,y);
-	}*/
-	void OnBackgroundLButtonDown(int x, int y)
-	//void OnLButtonDown(int x, int y)
-	{
-		if ( m_ctrlPtrAry.size() == 0 )
-			MessageBox("ccc");
-		else
-			MessageBox("aaa");
-		//inBitmap.Set(point.x, point.y, D3DCOLOR_WHITE);
-		//inBitmap.Set(point.x, point.y, D3DCOLOR_RED);
-		/*if ( D3DCOLOR_GETA(m_bitmap.Get(x, y)) != 0 )
-			m_bitmap.Fill(D3DCOLOR_RED,Rect(x, y, x+3, y+3));*/
-	}
-	void OnBackgroundMouseMove(int nPosX, int nPosY, int nMoveX, int nMoveY){
-		//MessageBox("aaa");
-		/*m_img.m_rect.top = nPosY;
-		m_img.m_rect.left = nPosX;*/
-		//m_img2.SetPos(nPosX, nPosY);
-	}
-
-
 };
 CMglTestFrame g_frame;
 
@@ -58,10 +27,6 @@ int APIENTRY WinMain(HINSTANCE hInstance,
                      LPSTR     lpCmdLine,
                      int       nCmdShow )
 {
-	//g_frame.EnableEscEnd();
-	//g_frame.StartWindow( 640, 480, (LPTHREAD_START_ROUTINE)MainThread,
-	//g_frame.StartWindow( 640, 480, (LPTHREAD_START_ROUTINE)MainThread );
 	g_frame.Start();
-
 	return 0;
 }
