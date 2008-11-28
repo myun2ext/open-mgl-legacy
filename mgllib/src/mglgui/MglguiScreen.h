185e11b086f8e92045093885f059c936da75a21a
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2008-11-28 19:47:30 +0000

    もいっちょおまけに MglguiWindow.h, MglguiWindow.cpp への移動。
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@246 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/mglgui/MglguiScreen.h
+++ mgllib/src/mglgui/MglguiScreen.h
@@ -135,62 +135,6 @@ public:
 	//bool OnFrameMouseInput(); <- なんかpublicなのに理由あんのかな・・・？
 };
 
-//////////// ウインドウ作成もやってくれるクラス ////////////////////
-
-
-
-
-#include "MglGraphicManager.h"
-#include "MglText.h"
-#include "MyuFPS.h"
-#include "MglInput.h"
-#include "MglAudio.h"
-
-/*class CMglguiWindowOnCreateExtend : public CCreateWindowInfoExtendBase
-{
-
-};*/
-typedef struct : agh::CREATE_WINDOW_EXTEND_BASE
-{
-	BOOL bFullScreen;
-	BOOL bEscKeyExit;
-	int nNotMoveHideCursolTimeMs;
-} MGLGUI_WINDOW_ON_CREATE_EXTEND;
-
-//	2008/11/13  CWindowBaseのDLLエクスポート
-class DLL_EXP agh::CWindowBase;
-
-//class DLL_EXP CMglguiWindow : public CMglguiScreen
-class DLL_EXP CMglguiWindow : public CMglguiScreen, public agh::CWindowBase
-{
-private:
-protected:
-	HINSTANCE m_hInstance;
-	HINSTANCE m_hPrevInstance;
-	LPSTR m_lpCmdLine;
-	int m_nCmdShow;
-
-	//	DirectX（MGL）系
-	/*CMglGraphicManager m__grp;
-	CMyuFPS m__fps;
-	CMglInput m__input;
-	CMglAudio m__audio;*/
-public:
-	bool __ThreadFunc();
-
-public:
-	void Start();
-	void Start(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
-		m_hInstance = hInstance;
-		m_hPrevInstance = m_hPrevInstance;
-		m_lpCmdLine = lpCmdLine;
-		m_nCmdShow = nCmdShow;
-		Start();
-	}
-	/*int StartWindow( int nWinWidthSize, int nWinHeightSize,
-		const char *szWindowTitle="MGL Application", BOOL bFullscreen=FALSE );*/
-};
-
 
 
 	/*virtual void OnBackgroundLButtonDown(int x, int y){}
--- mgllib/src/mglgui/MglguiWindow.h
+++ mgllib/src/mglgui/MglguiWindow.h
@@ -7,4 +7,59 @@
 #ifndef __MglguiWindow_H__
 #define __MglguiWindow_H__
 
+#include "MglguiScreen.h"
+#include "MglGraphicManager.h"
+#include "MglText.h"
+#include "MyuFPS.h"
+#include "MglInput.h"
+#include "MglAudio.h"
+
+/*class CMglguiWindowOnCreateExtend : public CCreateWindowInfoExtendBase
+{
+
+};*/
+typedef struct : agh::CREATE_WINDOW_EXTEND_BASE
+{
+	BOOL bFullScreen;
+	BOOL bEscKeyExit;
+	int nNotMoveHideCursolTimeMs;
+} MGLGUI_WINDOW_ON_CREATE_EXTEND;
+
+//	2008/11/13  CWindowBaseのDLLエクスポート
+class DLL_EXP agh::CWindowBase;
+
+//class DLL_EXP CMglguiWindow : public CMglguiScreen
+class DLL_EXP CMglguiWindow : public CMglguiScreen, public agh::CWindowBase
+{
+private:
+protected:
+	HINSTANCE m_hInstance;
+	HINSTANCE m_hPrevInstance;
+	LPSTR m_lpCmdLine;
+	int m_nCmdShow;
+
+	//	DirectX（MGL）系
+	/*CMglGraphicManager m__grp;
+	CMyuFPS m__fps;
+	CMglInput m__input;
+	CMglAudio m__audio;*/
+public:
+	bool __ThreadFunc();
+
+public:
+	void Start();
+	void Start(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow){
+		m_hInstance = hInstance;
+		m_hPrevInstance = m_hPrevInstance;
+		m_lpCmdLine = lpCmdLine;
+		m_nCmdShow = nCmdShow;
+		Start();
+	}
+	/*int StartWindow( int nWinWidthSize, int nWinHeightSize,
+		const char *szWindowTitle="MGL Application", BOOL bFullscreen=FALSE );*/
+};
+
+
+
+
 #endif//__MglguiWindow_H__
