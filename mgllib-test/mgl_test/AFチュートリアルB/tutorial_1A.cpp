c3ab98613836cadc3a1785985f1290f578efed4b
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2008-11-29 12:48:08 +0000

    チュートリアルBの1A追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@257 6d62ff88-fa28-0410-b5a4-834eb811a934

---
+++ mgllib-test/mgl_test/AFチュートリアルB/tutorial_1A.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow )
+{
+	CAugustWindow frame;
+	frame.Start();
+	return 0;
+}
