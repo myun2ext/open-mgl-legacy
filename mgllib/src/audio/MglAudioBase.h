64964b8c113eb239db31898221b9c96b50c4f9e4
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2008-12-04 16:31:20 +0000

    CMglAudioBase 追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@277 6d62ff88-fa28-0410-b5a4-834eb811a934

---
+++ mgllib/src/audio/MglAudioBase.h
@@ -0,0 +1,15 @@
+#ifndef __MglAudioBase_H__
+#define __MglAudioBase_H__
+
+//	ƒNƒ‰ƒXéŒ¾
+class DLL_EXP CMglAudioBase
+{
+private:
+
+public:
+
+};
+
+typedef CMglAudioBase IMglAudioBase;
+
+#endif//__MglAudioBase_H__
