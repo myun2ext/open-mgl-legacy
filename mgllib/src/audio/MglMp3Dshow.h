d3d27699f7626c31525cc29d0b1b8913c78477fe
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2008-12-06 11:52:42 +0000

    LoopPlay()実装してないのに何故かビルド出来ちゃったΣ
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@281 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/audio/MglMp3Dshow.h
+++ mgllib/src/audio/MglMp3Dshow.h
@@ -12,37 +12,8 @@
 #include "MglBgmBase.h"
 
 //	クラス宣言
-class DLL_EXP CMglMp3Dshow : public CMglDirectShowBase, public CMglBgmBase
+class DLL_EXP CMglMp3Dshow : public CMglDirectShowBase
 {
-private:
-	
-
-public:
-	CMglMp3Dshow();
-	virtual ~CMglMp3Dshow();
-
-	//	初期化/終端系
-	void Init();
-	void Release();
-	void Load( const char* szOggFile );
-	void UnLoad();
-	void Unload(){ UnLoad(); }
-
-	//	再生操作系
-	void Play( int nLoopCnt=VOX_LOOP_MAX );
-	void StopLoop();
-	void Pause();
-	void SetVolume( float fVolume=VOX_DEFAULT_VOLUME );
-	void Fade( float fTargetVolume, int nFadeTimeMs );
-	void FadeIn( int nFadeTimeMs ) { SetVolume(0); Fade( 1.0f, nFadeTimeMs ); }
-	void FadeOut( int nFadeTimeMs ) { Fade( 0, nFadeTimeMs ); }
-
-	void Enable() { m_useFlg = TRUE; }
-	void Disable() { m_useFlg = FALSE; }
 };
-
-//typedef CMglMp3Dshow CMglMp3Dshow;
-//class CMglMp3Dshow : public CMglMp3Dshow{};
-//typedef CMglMp3Dshow CMglMusic;
 
 #endif//__MglMp3Dshow_H__
