9b5c8e5117ca67c876fc8a48703e9da6948df741
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2008-12-07 13:34:44 +0000

    CMglAghImage: ScaleとかAngleにも対応
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@286 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/mglgui/MglAghImage.cpp
+++ mgllib/src/mglgui/MglAghImage.cpp
@@ -31,6 +31,7 @@ bool CMglAghImage::Load(const char* szImageFilePath)
 
 void CMglAghImage::OnDraw()
 {
-	_Img()->Draw( GetRect().left, GetRect().top, NULL, m_color );
+	_Img()->Draw( GetRect().left, GetRect().top,
+		NULL, m_color, m_fScaleX, m_fScaleY, 0.5f, 0.5f, m_nAngle);
 }
 
