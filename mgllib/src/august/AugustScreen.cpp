2ac99b45a90ab4af05d4520b0f2a57d0dc49246b
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2008-12-18 08:28:02 +0000

    CAugustScreen: iteratorを使って、各サブ・コントロールのOnFrame()呼び出すようにしてみた。
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@338 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/august/AugustScreen.cpp
+++ mgllib/src/august/AugustScreen.cpp
@@ -9,6 +9,7 @@
 #include "AugustScreen.h"
 
 using namespace agh;
+using namespace std;
 
 class DLL_EXP agh::CScreenBase;
 
@@ -53,6 +54,19 @@ bool CAugustScreen::DoFrame()
 	//	2008/11/02 ユーザイベント処理
 	if ( OnFrameDoUser() != true )
 		return false;
+
+	//	各コントロールのフレーム処理
+	//for(int i=0; i<m_ctrlPtrAry.size(); i++)
+	//for(int i=0; i<m_ctrlPtrAry.size(); _vcpp(i))
+	for(citr it=cbegin(); it != cend(); it++)
+	//for(vector<CControlBase*>::iterator it=cbegin(); it != cend(); it++)
+	{
+		it->OnFrame();
+		//(*it)->OnFrame();
+		//((CControlBase*)it)->OnFrame();
+		//m_ctrlPtrAry[i]->OnFrame();
+		//GetVCtrlPtr(i)->Draw();
+	}
 
 	return true;
 }
