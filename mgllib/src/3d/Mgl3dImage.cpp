ea0107f077fc3543eda183486bfd11e6a7c93e67
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-01-09 16:25:30 +0000

    CMgl3dImage: Draw() 嘘っﾊﾟﾁだから直しといた。
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@351 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/3d/Mgl3dImage.cpp
+++ mgllib/src/3d/Mgl3dImage.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 #include "Mgl3dImage.h"
 
-#define PI (3.1415926535f)
-
 //	BMPの頂点情報取得
 void CMgl3dImage::GetBmpVertexs3D( MGL_SQUARE_VERTEXS *pMglSqVertexs, float fScaleX, float fScaleY )
 {
@@ -37,42 +35,10 @@ void CMgl3dImage::GetBmpVertexs3D( MGL_SQUARE_VERTEXS *pMglSqVertexs, float fSca
 	//MglMoveVertexs( pMglSqVertexs, TEXTURE_FLOAT_ADJ, TEXTURE_FLOAT_ADJ );
 }
 
-
+//	描画
 void CMgl3dImage::Draw()
 {
- 	static int i=0;
-	i++;
 
-    D3DXMATRIX mWorld, mRotX, mRotY, mTrans;
-	float time = (float)timeGetTime();
-	//i = time/300;
-	//TODO: D3DXMatrixRotationY(&mRotY, time/600.0f);
-	//D3DXMatrixRotationY(&mRotY, 600.0f);
-	D3DXMatrixRotationY(&mRotY, 10.0f+i);
-//	D3DXMatrixRotationX(&mRotX, time/300.0f);
-	D3DXMatrixRotationX(&mRotX, 10.0f+i);
-	D3DXMatrixTranslation(&mTrans, 0,0,0.0f);
-	mWorld = mRotX * mRotY * mTrans;
-	D3DXMATRIX mView, mProj;
-	D3DXMatrixLookAtLH(&mView
-					,&D3DXVECTOR3(0-(i/100.0f),2.0f,2.0f-(i/100.0f))	// カメラ位置
-					,&D3DXVECTOR3(0+(i/1000.0f),0,0)		// カメラの注目点
-					,&D3DXVECTOR3(0,1,0)		// 上の向き
-					);
-	D3DXMatrixPerspectiveFovLH(&mProj
-		,60.0f*PI/180.0f						// 視野角
-		,1.0f									// アスペクト比
-		,0.01f									// 最近接距離
-		,100.0f									// 最遠方距離
-		);
-	d3d->SetTransform(D3DTS_WORLD, &mWorld);
-	d3d->SetTransform(D3DTS_VIEW,  &mView);
-	d3d->SetTransform(D3DTS_PROJECTION, &mProj);
-
-	//lpD3DDEV->SetStreamSource( 0, pVB, sizeof(CUSTOMVERTEX) );
-    d3d->SetVertexShader( FVF_MYU_VERTEX );
-    //lpD3DDEV->DrawPrimitive( D3DPT_TRIANGLEFAN, 0, 3 );
-	Draw();
 }
 
 
