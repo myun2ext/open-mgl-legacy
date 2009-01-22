92a472649d91ac7d8daa316cd30077f5fd2457e5
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-01-22 17:24:57 +0000

    CAugust3dManager: Add
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@416 6d62ff88-fa28-0410-b5a4-834eb811a934

---
+++ mgllib/src/august/August3dManager.cpp
@@ -0,0 +1,117 @@
+//////////////////////////////////////////////////////////
+//
+//	August3dManager
+//		- MglGraphicManager レイヤークラス
+//
+//////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "August3dManager.h"
+
+using namespace agh;
+using namespace std;
+
+class DLL_EXP agh::CScreenBase;
+
+/*
+//	コンストラクタ
+CAugust3dManager::CAugust3dManager()
+{
+}
+
+//	デストラクタ
+CAugust3dManager::~CAugust3dManager()
+{
+}
+*/
+
+
+//////////////////////////////////////////////////
+
+void CAugust3dManager::OnDraw()
+{
+	g_->pGrp->Clear(m_rgbBackground);	//	2008/11/29 CAugust3dManager対応
+
+	//for(int i=0; i<m_ctrlPtrAry.size(); i++)
+	for(int i=0; i<m_ctrlPtrAry.size(); _vcpp(i))
+	{
+		//m_ctrlPtrAry[i]->Draw();
+		GetVCtrlPtr(i)->Draw();
+	}
+}
+
+
+//	フレーム処理
+bool CAugust3dManager::DoFrame()
+{
+	//	2009/01/23 マウスのハンドルは無効に出来るように。
+	//	（結構処理長いし、うっかりクリックとかで余計な処理走るとまずいし・・・）
+	if ( m_bUseMouseHandle == true )
+	{
+		OnFrameMouseInput();	//	なんか拾わないとfalseを返す仕様になってるっぽい・・・
+		/*if ( OnFrameMouseInput() != true )
+			return false;*/
+	}
+
+	//	キーボードのハンドル処理
+	if ( OnFrameKeyboardInput() != true )
+		return false;
+
+	//	2008/11/02 ユーザイベント処理
+	if ( OnFrameDoUser() != true )
+		return false;
+
+	//	各コントロールのフレーム処理
+	//for(int i=0; i<m_ctrlPtrAry.size(); i++)
+	//for(int i=0; i<m_ctrlPtrAry.size(); _vcpp(i))
+	for(citr it=cbegin(); it != cend(); it++)
+	//for(vector<CControlBase*>::iterator it=cbegin(); it != cend(); it++)
+	{
+		it->OnFrame();
+		//(*it)->OnFrame();
+		//((CControlBase*)it)->OnFrame();
+		//m_ctrlPtrAry[i]->OnFrame();
+		//GetVCtrlPtr(i)->Draw();
+	}
+
+	return true;
+}
+
+
+/////////////////////////////////////////////////////////////
+
+//	Eventsへ。
+
+/////////////////////////////////////////////////////////////
+
+/*
+void CAugust3dManager::RegistControl(CMglAghImage* pImage)
+{
+	const char* szFilePath = pImage->m_strFilePath.c_str();
+	const char* szAlias = pImage->m_strLayerName.c_str();
+
+	//	せっとあっぷ
+	pImage->_Setup(g_->pGrp, g_->pImgCache);
+	
+	//	きゃっしゅ
+	g_->pImgCache->Cache(szFilePath);
+
+	pImage->SetRect(0,0,g_->pImgCache->Get(szFilePath)->GetBmpWidth(),
+		g_->pImgCache->Get(szFilePath)->GetBmpHeight());
+
+	//	そして登録
+	_RegistControl(pImage);
+	//m_layer.RegistBegin( new CMglImageLayer(m_imgCache[szFilePath]), true );
+}
+*/
+void CAugust3dManager::RegistControl(CAugustVisualControlBase* pControl)
+{
+	pControl->OnRegist(g_);
+
+	_RegistControlInternal(pControl);
+}
+
+void CAugust3dManager::_RegistControlInternal(agh::CControlBase* pCtrl)
+{
+	m_ctrlPtrAry.push_back(pCtrl);
+}
