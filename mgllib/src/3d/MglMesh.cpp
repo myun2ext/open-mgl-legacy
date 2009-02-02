6bc9689783c603a0f7bea5d527ba20e9c65911d6
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-02-02 17:58:41 +0000

    CMglMesh: CreateBox()がとりあえず表示されるようになったのと、Get系メソッド沢山追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@446 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/3d/MglMesh.cpp
+++ mgllib/src/3d/MglMesh.cpp
@@ -7,17 +7,14 @@ CMglMesh::CMglMesh()
 	m_pMesh          = NULL;
 	m_pMeshMaterials = NULL;
 	m_pMeshTextures  = NULL;
-	m_dwNumMaterials = 0L;
+	m_dwMaterialCount = 0L;
 }
 
 //	Xファイル読み込み
 void CMglMesh::Load(const char* szMeshFilePath)
 {
 	InitCheck();
-
-	if ( m_pMesh != NULL )
-		MyuThrow(MGLMSGNO_MESH(1), "CMglMesh::Load()  既に読み込み済みです。"
-			"再び読み込む場合には一度 Release() を呼び出した後読み込んでください。");
+	CreatedCheck();
 
 	//	ファイルが本当にあるかどうか？
 	if ( !msl::IsExistFile(szMeshFilePath) )
@@ -29,17 +26,17 @@ void CMglMesh::Load(const char* szMeshFilePath)
 	LPD3DXBUFFER pD3DXMtrlBuffer = NULL;
 	MyuAssert2( D3DXLoadMeshFromX( (char*)szMeshFilePath, D3DXMESH_SYSTEMMEM, 
 								   m_d3d, NULL, 
-								   &pD3DXMtrlBuffer, &m_dwNumMaterials, 
+								   &pD3DXMtrlBuffer, &m_dwMaterialCount, 
 								   &m_pMesh ), D3D_OK,
 		MGLMSGNO_MESH(4), "CMglMesh::Load()  D3DXLoadMeshFromX()に失敗" );
 
 	// pD3DXMtrlBuffer から、質感やテクスチャーの情報を読み取る
 	D3DXMATERIAL* d3dxMaterials = (D3DXMATERIAL*)pD3DXMtrlBuffer->GetBufferPointer();
-	m_pMeshMaterials = new _D3DMATERIALx[m_dwNumMaterials];
-	m_pMeshTextures  = new _IDirect3DTextureX* [m_dwNumMaterials];
-	ZeroMemory(m_pMeshTextures, sizeof(LPVOID*)*m_dwNumMaterials);
+	m_pMeshMaterials = new _D3DMATERIALx[m_dwMaterialCount];
+	m_pMeshTextures  = new _IDirect3DTextureX* [m_dwMaterialCount];
+	ZeroMemory(m_pMeshTextures, sizeof(LPVOID*)*m_dwMaterialCount);
 
-	for( DWORD i=0; i < m_dwNumMaterials; i++ )
+	for( DWORD i=0; i < m_dwMaterialCount; i++ )
 	{
 		m_pMeshMaterials[i] = d3dxMaterials[i].MatD3D;// 質感のコピー
 		m_pMeshMaterials[i].Ambient = m_pMeshMaterials[i].Diffuse;// マテリアルの環境色を設定する
@@ -90,7 +87,7 @@ void CMglMesh::Release()
 	//	テクスチャの解放
 	if( m_pMeshTextures )
 	{
-		for( DWORD i=0; i < m_dwNumMaterials; i++ )
+		for( DWORD i=0; i < m_dwMaterialCount; i++ )
 			SAFE_RELEASE( m_pMeshTextures[i] );
 	
 		SAFE_DELETE_ARRAY( m_pMeshTextures );
@@ -105,7 +102,7 @@ void CMglMesh::Draw()
 {
 	InitCheck();
 
-	for( DWORD i=0; i < m_dwNumMaterials; i++ )
+	for( DWORD i=0; i < m_dwMaterialCount; i++ )
 	{
 		if ( m_pMeshMaterials != NULL )
 			m_pD3dDev->SetMaterial( &m_pMeshMaterials[i] );
@@ -119,9 +116,51 @@ void CMglMesh::Draw()
 
 /////////////////////////////////////////////////
 
-void CMglMesh::CreateBox(float fWidth, float fHeight, float fDepth)
+//	箱メッシュ作成
+void CMglMesh::CreateBox(float fWidth, float fHeight, float fDepth,
+		D3DCOLOR color, D3DCOLOR ambient, D3DCOLOR specular, float fPower)
 {
+	InitCheck();
+	CreatedCheck();
+
 	MyuAssert2( D3DXCreateBox( m_d3d, fWidth, fHeight, fDepth, &m_pMesh, NULL), D3D_OK,
 		MGLMSGNO_MESH(32), "CMglMesh::CreateBox()  D3DXCreateBox()に失敗" );
+
+	///////////////////////////////////////////////////////////////
+
+	//m_dwMaterialCount = 1;
+	CreateMaterials(1);
+
+	_D3DMATERIALx* pMat = this->GetMaterial(0);
+	/*pMat->Ambient = D3DXCOLOR(1.0f,1.0f,1.0f,1.0f);
+	pMat->Diffuse = D3DXCOLOR(1.0f,1.0f,1.0f,1.0f);
+	pMat->Specular = D3DXCOLOR(1.0f,1.0f,1.0f,1.0f);
+	pMat->Power = 20.0f;*/
+	pMat->Diffuse = D3DXCOLOR(color);
+	pMat->Ambient = D3DXCOLOR(ambient);
+	pMat->Specular = D3DXCOLOR(specular);
+	pMat->Power = 20.0f;
+
+	/*
+	m_pMeshMaterials = new _D3DMATERIALx[m_dwMaterialCount];
+	m_pMeshTextures  = new _IDirect3DTextureX* [m_dwMaterialCount];
+	ZeroMemory(m_pMeshTextures, sizeof(LPVOID*)*m_dwMaterialCount);
+
+	for( DWORD i=0; i < m_dwMaterialCount; i++ )
+	{
+		m_pMeshMaterials[i] = d3dxMaterials[i].MatD3D;// 質感のコピー
+		m_pMeshMaterials[i].Ambient = m_pMeshMaterials[i].Diffuse;// マテリアルの環境色を設定する
+	 
+		const char* szTextureFile = d3dxMaterials[i].pTextureFilename;
+		if ( szTextureFile != NULL )
+		{
+			MyuAssert2( D3DXCreateTextureFromFile( m_d3d, 
+											szTextureFile, 
+											&m_pMeshTextures[i] ), D3D_OK,
+				MGLMSGNO_MESH(5), "CMglMesh::Load()  D3DXCreateTextureFromFile(\"%s\")に失敗",
+				szTextureFile);
+		}
+	}
+	*/
 }
 
--- mgllib/src/3d/MglMesh.h
+++ mgllib/src/3d/MglMesh.h
@@ -17,7 +17,26 @@ protected:
 	ID3DXMesh*				m_pMesh;			// メッシュ
 	_D3DMATERIALx*			m_pMeshMaterials;	// メッシュの質感
 	_IDirect3DTextureX**	m_pMeshTextures;	// メッシュのテクスチャ群
-	DWORD					m_dwNumMaterials;	// マテリアルの数
+	DWORD					m_dwMaterialCount;	// マテリアルの数
+
+	//-------------------------------------------------------------
+
+	void CreatedCheck(){
+		if ( m_pMesh != NULL )
+			MyuThrow(MGLMSGNO_MESH(0), "CMglMesh::Load()  既に読み込み済みです。"
+				"再び読み込む場合には一度 Release() を呼び出した後読み込んでください。");
+	}
+	void CreateMaterials(){ CreateMaterials(m_dwMaterialCount); }
+	void CreateMaterials(int nCount){
+		m_pMeshMaterials = new _D3DMATERIALx[nCount];
+		m_dwMaterialCount = nCount;
+	}
+	void CreateTextures(){ CreateTextures(m_dwMaterialCount); }
+	void CreateTextures(int nCount){
+		m_pMeshTextures = new _IDirect3DTextureX* [nCount];
+		ZeroMemory(m_pMeshTextures, sizeof(LPVOID*)*nCount);
+		m_dwMaterialCount = nCount;
+	}
 
 public:
 	CMglMesh();
@@ -28,9 +47,22 @@ public:
 	void LoadXFile(const char* szXFilePath){ Load(szXFilePath); }
 	void Create(const char* szMeshFilePath){ Load(szMeshFilePath); }
 
-	void CreateBox(float fWidth, float fHeight, float fDepth);
-
 	void Draw();
+
+	//	自動メッシュ
+	void CreateBox(float fWidth, float fHeight, float fDepth,
+		D3DCOLOR color=D3DCOLOR_WHITE, D3DCOLOR ambient=D3DCOLOR_WHITE,
+		D3DCOLOR specular=D3DCOLOR_WHITE, float fPower=1.0f);
+
+	//-------------------------------------------
+
+	ID3DXMesh* GetMeshPtr(){ return m_pMesh; }
+	ID3DXMesh* GetID3DXMeshPtr(){ return m_pMesh; }
+	_D3DMATERIALx* GetMaterialArrayPtr(){ return m_pMeshMaterials; }
+	_IDirect3DTextureX** GetTextureArrayPtr(){ return m_pMeshTextures; }
+	DWORD GetMaterialCount(){ return m_dwMaterialCount; }
+
+	_D3DMATERIALx* GetMaterial(int index){ return &m_pMeshMaterials[index]; }
 };
 
 typedef CMglMesh CMgl3dMesh;
