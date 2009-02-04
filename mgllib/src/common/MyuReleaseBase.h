68c0f92c3b06ff65b1d4c0df3fba452d0fe50012
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-02-04 01:52:00 +0000

    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@458 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/common/MyuReleaseBase.h
+++ mgllib/src/common/MyuReleaseBase.h
@@ -2,12 +2,45 @@
 #define __MyuReleaseBase_H__
 
 //	クラス宣言
-class DLL_EXP CMyuReleaseBase
+class IMyuReleasable // 特に現状エクスポートするものは無いんだが、将来的な事を見越してDLL_EXPつけとく
 {
 private:
 
 public:
 	virtual void Release()=0;
 };
+
+//	クラス宣言
+class CMyuReleaseBase : public IMyuReleasable
+{
+private:
+
+public:
+	//virtual ~CMyuReleaseBase(){ Release(); }
+};
+
+/////////////////////////////////////////////////////////////////////////////////
+/*
+//	クラス宣言
+class DLL_EXP IMyuReleasable2 // 特に現状エクスポートするものは無いんだが、将来的な事を見越してDLL_EXPつけとく
+{
+private:
+
+public:
+	virtual void Release()=0;
+};
+
+//	クラス宣言
+class CMyuReleaseBase2 : public IMyuReleasable2
+{
+private:
+
+public:
+	virtual ~CMyuReleaseBase2(){ Release(); }
+};
+*/
+
+//typedef CMyuReleaseBase CMyuReleasable, CMyuReleaseable, CMglReleasable, CMglReleaseable;
+//typedef IMyuReleaseBase IMyuReleasable, IMyuReleaseable, IMglReleasable, IMglReleaseable;
 
 #endif//__MyuReleaseBase_H__
