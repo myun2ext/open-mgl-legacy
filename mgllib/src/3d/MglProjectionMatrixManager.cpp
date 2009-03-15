5de9e30a00e6ea66993d2ab3704b717a0f8e08c9
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-03-15 14:32:23 +0000

    とりあえず CMglProjectionMatrixManager をまともに作ってみた。
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@621 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/3d/MglProjectionMatrixManager.cpp
+++ mgllib/src/3d/MglProjectionMatrixManager.cpp
@@ -11,24 +11,10 @@
 //	コンストラクタ
 CMglProjectionMatrixManager::CMglProjectionMatrixManager()
 {
-	m_fAspectRatio = 640.0f/480;
+	m_fAspectRatio = 640.0f/480.0f;
 	m_fViewingAngle = 45.0f;
 	m_fClipNear = 0.01f;
 	m_fClipFar = 100.0f;
-
-	m_fCameraPosX = 0;
-	m_fCameraPosY = 0;
-	m_fCameraPosZ = 0;
-	m_fCameraTargetX = 0;
-	m_fCameraTargetY = 0;
-	m_fCameraTargetZ = 0;
-
-	m_fRotateX = 0;
-	m_fRotateY = 0;
-	m_fRotateZ = 0;
-	m_fMoveX = 0;
-	m_fMoveY = 0;
-	m_fMoveZ = 0;
 }
 
 //	デストラクタ
@@ -48,9 +34,6 @@ void CMglProjectionMatrixManager::Init( CMglGraphicManager* in_myudg )
 
 	//	Projectionの設定
 	SetupProjection( (m_myudg->GetWidth()*1.0f) / m_myudg->GetHeight());
-
-	//	とりあえずデフォルトなカメラを設定
-	SetCamera(0,0,-5.0f, 0,0,0);
 }
 
 //	Projectionの設定
@@ -84,191 +67,6 @@ void CMglProjectionMatrixManager::SetupProjection( float fAspectRatio, float fVi
 //D3D_COORDINATE	g_WorldFrame(D3DXVECTOR3(1,1,1), D3DXVECTOR3(0,0,0), D3DXVECTOR3(0,0,0));
 }
 
-////////////////////////////////////////////////////////////
-
-void CMglProjectionMatrixManager::SetCamera(D3DXMATRIX &matTarget, D3DXMATRIX& matPos, float fRotate)
-{
-	m_matTarget = matTarget;
-	m_matPos = matPos;
-	SetCamera(
-		D3DXMatrixToVector3(&matTarget),
-		D3DXMatrixToVector3(&matPos), fRotate);
-}
-
-void CMglProjectionMatrixManager::SetCamera(D3DXVECTOR3 &vecTarget, D3DXVECTOR3 &vecPos, float fRotate)
-{
-	m_vecTarget = vecTarget;
-	m_vecPos = vecPos;
-	SetCamera(
-		vecPos.x, vecPos.y, vecPos.z,
-		vecTarget.x, vecTarget.y, vecTarget.z,
-		fRotate);
-}
-
-void CMglProjectionMatrixManager::SetCamera(
-	float fPosX, float fPosY, float fPosZ,
-	float fTargetX, float fTargetY, float fTargetZ,
-	float fRotate)
-{
-	InitCheck();
-	CameraLockAt(fPosX, fPosY, fPosZ, fTargetX, fTargetY, fTargetZ, fRotate);
-
-	m_fCameraPosX = fPosX;
-	m_fCameraPosY = fPosY;
-	m_fCameraPosZ = fPosZ;
-	m_fCameraTargetX = fTargetX;
-	m_fCameraTargetY = fTargetY;
-	m_fCameraTargetZ = fTargetZ;
-}
-
-//	カメラ位置の設定
-//void CMglProjectionMatrixManager::SetCameraPos(float x, float y, float z)
-void CMglProjectionMatrixManager::CameraLockAt(float fPosX, float fPosY, float fPosZ, float fTargetX, float fTargetY, float fTargetZ, float fRotate)
-{
-	InitCheck();
-#if _MGL3D_COORDINATE_USE == _MGL3D_COORDINATE_LEFT_HAND
-	D3DXMatrixLookAtLH(
-#else
-	D3DXMatrixLookAtRH(
-#endif
-					&m_matView
-					,&D3DXVECTOR3(fPosX, fPosY, fPosZ)	// カメラ位置
-					,&D3DXVECTOR3(fTargetX, fTargetY, fTargetZ)		// カメラの注目点
-					,&D3DXVECTOR3(0,1,0)		// 上の向き
-					);
-
-	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &m_matView), D3D_OK,
-		"CMglProjectionMatrixManager::SetCamera()  SetTransform()に失敗" );
-}
-
-//	カメラの位置を変更
-void CMglProjectionMatrixManager::SetCameraPos(float x, float y, float z)
-{
-	SetCamera(x, y, z, m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ);
-}
-//	カメラの注目点を変更
-void CMglProjectionMatrixManager::SetCameraViewTarget(float x, float y, float z)
-{
-	SetCamera(m_fCameraPosX, m_fCameraPosY, m_fCameraPosZ, x, y, z);
-}
-//	カメラを移動（シフト）
-void CMglProjectionMatrixManager::MoveCamera(float x, float y, float z)
-{
-	SetCamera(
-		m_fCameraPosX+x, m_fCameraPosY+y, m_fCameraPosZ+z,
-		m_fCameraTargetX+x, m_fCameraTargetY+y, m_fCameraTargetZ+z);
-}
-
-//	カメラをX軸方向に回転
-void CMglProjectionMatrixManager::CameraRotation(int direction, float fAngle)
-{
-	InitCheck();
-
-	float rad;
-
-	switch(direction){
-	//	横軸
-	case MGL3D_X:
-		m_fRotateX += fAngle;
-		rad = D3DXToRadian(m_fRotateX);
-		CameraLockAt(
-				m_fCameraPosX+(sin(rad)*(-m_fCameraPosZ)),
-				m_fCameraPosY,
-				m_fCameraPosZ+(1.0f-cos(rad))*(-m_fCameraPosZ),
-				m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ, 0.0f);
-		break;
-
-	//	縦軸
-	case MGL3D_Y:
-		m_fRotateY += fAngle;
-		rad = D3DXToRadian(m_fRotateY);
-		CameraLockAt(
-				m_fCameraPosX,
-				m_fCameraPosY+(sin(rad)*(-m_fCameraPosZ)),
-				m_fCameraPosZ+(1.0f-cos(rad))*(-m_fCameraPosZ),
-				m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ, 0.0f);
-		break;
-
-	//	Z軸
-	case MGL3D_Z:
-		m_fRotateY += fAngle;
-		rad = D3DXToRadian(m_fRotateY);
-		CameraLockAt(
-				m_fCameraPosX+(1.0f-cos(rad))*(-m_fCameraPosZ),
-				m_fCameraPosY+(sin(rad)*(-m_fCameraPosZ)),
-				m_fCameraPosZ,
-				m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ, 0.0f);
-		break;
-	}
-	/*
-	D3DXMATRIX matRotation;
-	switch(direction){
-	case MGL3D_X:
-		m_fRotateX += fAngle;
-		D3DXMatrixRotationX(&matRotation, D3DXToRadian(m_fRotateX));
-		break;
-	case MGL3D_Y:
-		m_fRotateY += fAngle;
-		D3DXMatrixRotationY(&matRotation, D3DXToRadian(m_fRotateY));
-		break;
-	case MGL3D_Z:
-		m_fRotateZ += fAngle;
-		D3DXMatrixRotationZ(&matRotation, D3DXToRadian(m_fRotateZ));
-		break;
-	}
-
-	//matRotation *= m_matView;
-	D3DXMATRIX mat;
-	D3DXMatrixMultiply(&mat, &m_matView, &matRotation);
-
-	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &mat), D3D_OK,
-		"CMglProjectionMatrixManager::SetCamera()  SetTransform()に失敗" );
-	*/
-}
-
-void CMglProjectionMatrixManager::SetCameraAngle(float fAngleX, float fAngleY, float fAngleZ)
-{
-	InitCheck();
-	float radX = D3DXToRadian(fAngleX);
-	float radY = D3DXToRadian(fAngleY);
-	float radZ = D3DXToRadian(fAngleZ);
-
-	CameraLockAt(
-		m_fCameraPosX+(sin(radX)*(-m_fCameraPosZ)),
-		m_fCameraPosY+(sin(radY)*(-m_fCameraPosZ)),
-		m_fCameraPosZ+(1.0f-cos(radX))*(-m_fCameraPosZ),
-		m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ, 0.0f);
-}
-
-void CMglProjectionMatrixManager::Rotate(float fAngleX, float fAngleY, float fAngleZ)
-{
-	m_fRotateX += fAngleX;
-	m_fRotateY += fAngleY;
-	m_fRotateZ += fAngleZ;
-}
-
-void CMglProjectionMatrixManager::ReTransform()
-{
-	InitCheck();
-
-	//	View（カメラ）
-	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &m_matView), D3D_OK,
-		"CMglProjectionMatrixManager::ReTransform()  SetTransform(D3DTS_VIEW)に失敗" );
-
-	//	プロジェクション
-	MyuAssert( m_pD3dDev->SetTransform(D3DTS_PROJECTION, &m_projection), D3D_OK,
-		"CMglProjectionMatrixManager::SetupProjection()  SetTransform()に失敗" );
-}
-
-//	View設定
-void CMglProjectionMatrixManager::SetViewMatrix(D3DXMATRIX &matView)
-{
-	InitCheck();
-	m_matView = matView;
-	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &m_matView), D3D_OK,
-		"CMglProjectionMatrixManager::ReTransform()  SetTransform(D3DTS_VIEW)に失敗" );
-}
-
 //	Projection設定
 void CMglProjectionMatrixManager::SetProjectionMatrix(D3DXMATRIX &matProjection)
 {
@@ -277,26 +75,3 @@ void CMglProjectionMatrixManager::SetProjectionMatrix(D3DXMATRIX &matProjection)
 	MyuAssert( m_pD3dDev->SetTransform(D3DTS_PROJECTION, &m_projection), D3D_OK,
 		"CMglProjectionMatrixManager::SetupProjection()  SetTransform()に失敗" );
 }
-
-///////////////////////////////////////////////////////////////////////////////////
-
-/*	↓移動するだけで全然駄目・・・
-//	カメラをX軸方向に回転
-void CMglProjectionMatrixManager::SetCameraAngle2(float fAngleX, float fAngleY, float fAngleZ)
-{
-	D3DXMATRIX matX;
-	D3DXMATRIX matY;
-	D3DXMATRIX matZ;
-	D3DXMatrixRotationX(&matX, D3DXToRadian(fAngleX));
-	D3DXMatrixRotationY(&matY, D3DXToRadian(fAngleY));
-	D3DXMatrixRotationZ(&matZ, D3DXToRadian(fAngleZ));
-
-	D3DXMATRIX mat = m_matView * matX * matY * matZ;
-	/*D3DXMatrixMultiply(&mat, &m_matView, &matRotation);
-	D3DXMatrixMultiply(&mat, &m_matView, &matRotation);
-	D3DXMatrixMultiply(&mat, &m_matView, &matRotation);*
-
-	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &mat), D3D_OK,
-		"CMglProjectionMatrixManager::SetCamera()  SetTransform()に失敗" );
-}
-*/
--- mgllib/src/3d/MglProjectionMatrixManager.h
+++ mgllib/src/3d/MglProjectionMatrixManager.h
@@ -23,42 +23,12 @@ class DLL_EXP CMglProjectionMatrixManager : public virtual CMglDgBase
 private:
 
 protected:
-	D3DXMATRIX m_matView;
 	D3DXMATRIX m_projection;
-
-
-	//	記憶
-	D3DXMATRIX m_matTarget;
-	D3DXMATRIX m_matPos;
-	D3DXVECTOR3 m_vecTarget;
-	D3DXVECTOR3 m_vecPos;
-
-	//////////////////////////////////
 
 	float m_fAspectRatio;
 	float m_fViewingAngle;
 	float m_fClipNear;
 	float m_fClipFar;
-
-	float m_fCameraPosX;
-	float m_fCameraPosY;
-	float m_fCameraPosZ;
-	float m_fCameraTargetX;
-	float m_fCameraTargetY;
-	float m_fCameraTargetZ;
-
-	float m_fRotateX;
-	float m_fRotateY;
-	float m_fRotateZ;
-	float m_fMoveX;
-	float m_fMoveY;
-	float m_fMoveZ;
-
-	void CameraLockAt(float fPosX, float fPosY, float fPosZ, float fTargetX, float fTargetY, float fTargetZ, float fRotate);
-
-	//	とりあえず制限に・・・
-	void CameraRotation(int direction, float fAngle);
-
 public:
 
 	//////////////////////////
@@ -80,34 +50,11 @@ public:
 
 	//	Projection
 	void SetupProjection( float fAspectRatio, float fViewingAngle=30.0f, float fClipNear=0.01f, float fClipFar=100.0f );
-	
-	//	カメラ
-	void SetCamera(float fPosX, float fPosY, float fPosZ, float fTargetX, float fTargetY, float fTargetZ, float fRotate=0.0f);
-	void SetCamera(D3DXMATRIX &matTarget, D3DXMATRIX& matPos, float fRotate=0.0f);
-	void SetCamera(D3DXVECTOR3 &vecTarget, D3DXVECTOR3 &vecPos, float fRotate=0.0f);
-
-	void SetCameraCenter(float x, float y, float z){ SetCameraViewTarget(x,y,z); }
-	void SetCameraViewTarget(float x, float y, float z);
-	void MoveCamera(float x, float y, float z);
-	void SetCameraAngle(float fAngleX, float fAngleY, float fAngleZ);
-	void SetCameraAngle2(float fAngleX, float fAngleY, float fAngleZ);
-	void SetCameraPos(float x, float y, float z);
-	void SetCameraPosition(float x, float y, float z){ SetCameraPos(x,y,z); }
-
-	void RotateX(float fAngle){ Rotate(fAngle,0,0); }
-	void RotateY(float fAngle){ Rotate(0,fAngle,0); }
-	void RotateZ(float fAngle){ Rotate(0,0,fAngle); }
-	void Rotate(float fAngleX, float fAngleY, float fAngleZ);
-
-	//	各Matrixの設定
-	/*void SetWorldMatrix(D3DXMATRIX &matWorld){ m_matWorld = matWorld; }
-	void SetViewMatrix(D3DXMATRIX &matView){ m_matView = matView; }
-	void SetProjectionMatrix(D3DXMATRIX &matProjection){ m_projection = matProjection; }*/
-	void SetViewMatrix(D3DXMATRIX &matView);
 	void SetProjectionMatrix(D3DXMATRIX &matProjection);
 
-	D3DXMATRIX& GetViewMatrix(){ return m_matView; }
 	D3DXMATRIX& GetProjectionMatrix(){ return m_projection; }
 };
+
+typedef CMglProjectionMatrixManager CMglPerspectiveMatrixManager;
 
 #endif//__MglProjectionMatrixManager_H__
