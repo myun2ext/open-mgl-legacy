dbc606cf601f11cff9e854d2a5bc4c94c28d79ef
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-03-15 15:36:18 +0000

    途中だけど・・・とりあえずcommitしちゃうー
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@624 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/3d/MglCameraMatrixManager.cpp
+++ mgllib/src/3d/MglCameraMatrixManager.cpp
@@ -11,19 +11,7 @@
 //	コンストラクタ
 CMglCameraMatrixManager::CMglCameraMatrixManager()
 {
-	m_fCameraPosX = 0;
-	m_fCameraPosY = 0;
-	m_fCameraPosZ = 0;
-	m_fCameraTargetX = 0;
-	m_fCameraTargetY = 0;
-	m_fCameraTargetZ = 0;
-
-	m_fRotateX = 0;
-	m_fRotateY = 0;
-	m_fRotateZ = 0;
-	m_fMoveX = 0;
-	m_fMoveY = 0;
-	m_fMoveZ = 0;
+	//m_fRotate = 0.0f;
 }
 
 //	デストラクタ
@@ -60,162 +48,67 @@ void CMglCameraMatrixManager::SetCamera(D3DXVECTOR3 &vecTarget, D3DXVECTOR3 &vec
 {
 	m_vecTarget = vecTarget;
 	m_vecPos = vecPos;
-	SetCamera(
+
+	CameraLockAt(vecPos, vecTarget, D3DXVECTOR3(0,1,0));
+	/*SetCamera(
 		vecPos.x, vecPos.y, vecPos.z,
 		vecTarget.x, vecTarget.y, vecTarget.z,
-		fRotate);
+		fRotate);*/
 }
 
-void CMglCameraMatrixManager::SetCamera(
-	float fPosX, float fPosY, float fPosZ,
-	float fTargetX, float fTargetY, float fTargetZ,
-	float fRotate)
+/////////////////////////////////////////////////////////////
+
+void CMglCameraMatrixManager::SetTargetPos(D3DXVECTOR3 &vec){}
+void CMglCameraMatrixManager::SetCameraPos(D3DXVECTOR3 &vec){}
+void CMglCameraMatrixManager::MoveTarget(D3DXVECTOR3 &vec){}
+void CMglCameraMatrixManager::MoveCamera(D3DXVECTOR3 &vec){}
+
+void CMglCameraMatrixManager::SetCameraAngle(D3DXVECTOR3 &vecAngles){}
+void CMglCameraMatrixManager::Rotate(float fAngleX, float fAngleY, float fAngleZ){}
+
+void CMglCameraMatrixManager::ReCreateViewMatrix()
 {
-	InitCheck();
-	CameraLockAt(fPosX, fPosY, fPosZ, fTargetX, fTargetY, fTargetZ, fRotate);
 
-	m_fCameraPosX = fPosX;
-	m_fCameraPosY = fPosY;
-	m_fCameraPosZ = fPosZ;
-	m_fCameraTargetX = fTargetX;
-	m_fCameraTargetY = fTargetY;
-	m_fCameraTargetZ = fTargetZ;
+	//SetCamera();
+	//CameraLockAt();
 }
 
-//	カメラ位置の設定
-//void CMglCameraMatrixManager::SetCameraPos(float x, float y, float z)
-void CMglCameraMatrixManager::CameraLockAt(float fPosX, float fPosY, float fPosZ, float fTargetX, float fTargetY, float fTargetZ, float fRotate)
-{
-	InitCheck();
-#if _MGL3D_COORDINATE_USE == _MGL3D_COORDINATE_LEFT_HAND
-	D3DXMatrixLookAtLH(
-#else
-	D3DXMatrixLookAtRH(
-#endif
-					&m_matView
-					,&D3DXVECTOR3(fPosX, fPosY, fPosZ)	// カメラ位置
-					,&D3DXVECTOR3(fTargetX, fTargetY, fTargetZ)		// カメラの注目点
-					,&D3DXVECTOR3(0,1,0)		// 上の向き
-					);
+////////////////////////////////////////////////////////////
 
-	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &m_matView), D3D_OK,
-		"CMglCameraMatrixManager::SetCamera()  SetTransform()に失敗" );
-}
-
-//	カメラの位置を変更
-void CMglCameraMatrixManager::SetCameraPos(float x, float y, float z)
-{
-	SetCamera(x, y, z, m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ);
-}
-//	カメラの注目点を変更
-void CMglCameraMatrixManager::SetCameraViewTarget(float x, float y, float z)
-{
-	SetCamera(m_fCameraPosX, m_fCameraPosY, m_fCameraPosZ, x, y, z);
-}
-//	カメラを移動（シフト）
-void CMglCameraMatrixManager::MoveCamera(float x, float y, float z)
-{
-	SetCamera(
-		m_fCameraPosX+x, m_fCameraPosY+y, m_fCameraPosZ+z,
-		m_fCameraTargetX+x, m_fCameraTargetY+y, m_fCameraTargetZ+z);
-}
-
-//	カメラをX軸方向に回転
-void CMglCameraMatrixManager::CameraRotation(int direction, float fAngle)
-{
-	InitCheck();
-
-	float rad;
-
-	switch(direction){
-	//	横軸
-	case MGL3D_X:
-		m_fRotateX += fAngle;
-		rad = D3DXToRadian(m_fRotateX);
-		CameraLockAt(
-				m_fCameraPosX+(sin(rad)*(-m_fCameraPosZ)),
-				m_fCameraPosY,
-				m_fCameraPosZ+(1.0f-cos(rad))*(-m_fCameraPosZ),
-				m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ, 0.0f);
-		break;
-
-	//	縦軸
-	case MGL3D_Y:
-		m_fRotateY += fAngle;
-		rad = D3DXToRadian(m_fRotateY);
-		CameraLockAt(
-				m_fCameraPosX,
-				m_fCameraPosY+(sin(rad)*(-m_fCameraPosZ)),
-				m_fCameraPosZ+(1.0f-cos(rad))*(-m_fCameraPosZ),
-				m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ, 0.0f);
-		break;
-
-	//	Z軸
-	case MGL3D_Z:
-		m_fRotateY += fAngle;
-		rad = D3DXToRadian(m_fRotateY);
-		CameraLockAt(
-				m_fCameraPosX+(1.0f-cos(rad))*(-m_fCameraPosZ),
-				m_fCameraPosY+(sin(rad)*(-m_fCameraPosZ)),
-				m_fCameraPosZ,
-				m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ, 0.0f);
-		break;
-	}
-	/*
-	D3DXMATRIX matRotation;
-	switch(direction){
-	case MGL3D_X:
-		m_fRotateX += fAngle;
-		D3DXMatrixRotationX(&matRotation, D3DXToRadian(m_fRotateX));
-		break;
-	case MGL3D_Y:
-		m_fRotateY += fAngle;
-		D3DXMatrixRotationY(&matRotation, D3DXToRadian(m_fRotateY));
-		break;
-	case MGL3D_Z:
-		m_fRotateZ += fAngle;
-		D3DXMatrixRotationZ(&matRotation, D3DXToRadian(m_fRotateZ));
-		break;
-	}
-
-	//matRotation *= m_matView;
-	D3DXMATRIX mat;
-	D3DXMatrixMultiply(&mat, &m_matView, &matRotation);
-
-	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &mat), D3D_OK,
-		"CMglCameraMatrixManager::SetCamera()  SetTransform()に失敗" );
-	*/
-}
-
-void CMglCameraMatrixManager::SetCameraAngle(float fAngleX, float fAngleY, float fAngleZ)
-{
-	InitCheck();
-	float radX = D3DXToRadian(fAngleX);
-	float radY = D3DXToRadian(fAngleY);
-	float radZ = D3DXToRadian(fAngleZ);
-
-	CameraLockAt(
-		m_fCameraPosX+(sin(radX)*(-m_fCameraPosZ)),
-		m_fCameraPosY+(sin(radY)*(-m_fCameraPosZ)),
-		m_fCameraPosZ+(1.0f-cos(radX))*(-m_fCameraPosZ),
-		m_fCameraTargetX, m_fCameraTargetY, m_fCameraTargetZ, 0.0f);
-}
-
-void CMglCameraMatrixManager::Rotate(float fAngleX, float fAngleY, float fAngleZ)
-{
-	m_fRotateX += fAngleX;
-	m_fRotateY += fAngleY;
-	m_fRotateZ += fAngleZ;
-}
-
+//	ReTransform
 void CMglCameraMatrixManager::ReTransform()
 {
 	InitCheck();
+
+	//	2009/03/16 なんでReTransformの時にやんないんだろ。なんか意味あんのかな・・・？
+	//ReCreateViewMatrix(); <- そっか・・・直接SetCamera()呼ばれた時のためか・・・
 
 	//	View（カメラ）
 	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &m_matView), D3D_OK,
 		"CMglCameraMatrixManager::ReTransform()  SetTransform(D3DTS_VIEW)に失敗" );
 }
+
+//	カメラ位置の設定
+void CMglCameraMatrixManager::ReCreateViewMatrix(D3DXVECTOR3 &vecEye, D3DXVECTOR3 &vecAt, D3DXVECTOR3 &vecUp)
+{
+	InitCheck();
+
+#if _MGL3D_COORDINATE_USE == _MGL3D_COORDINATE_LEFT_HAND
+	D3DXMatrixLookAtLH(
+#else
+	D3DXMatrixLookAtRH(
+#endif
+					&m_matView, &vecEye, &vecAt, &vecUp
+//					,&D3DXVECTOR3(fPosX, fPosY, fPosZ)	// カメラ位置
+//					,&D3DXVECTOR3(fTargetX, fTargetY, fTargetZ)		// カメラの注目点
+//					,&D3DXVECTOR3(0,1,0)		// 上の向き
+					);
+
+	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &m_matView), D3D_OK,
+		"CMglCameraMatrixManager::ReCreateViewMatrix()  SetTransform()に失敗" );
+}
+
+/////////////////////////////////////////////////////////////////////
 
 //	View設定
 void CMglCameraMatrixManager::SetViewMatrix(D3DXMATRIX &matView)
@@ -225,26 +118,3 @@ void CMglCameraMatrixManager::SetViewMatrix(D3DXMATRIX &matView)
 	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &m_matView), D3D_OK,
 		"CMglCameraMatrixManager::ReTransform()  SetTransform(D3DTS_VIEW)に失敗" );
 }
-
-///////////////////////////////////////////////////////////////////////////////////
-
-/*	↓移動するだけで全然駄目・・・
-//	カメラをX軸方向に回転
-void CMglCameraMatrixManager::SetCameraAngle2(float fAngleX, float fAngleY, float fAngleZ)
-{
-	D3DXMATRIX matX;
-	D3DXMATRIX matY;
-	D3DXMATRIX matZ;
-	D3DXMatrixRotationX(&matX, D3DXToRadian(fAngleX));
-	D3DXMatrixRotationY(&matY, D3DXToRadian(fAngleY));
-	D3DXMatrixRotationZ(&matZ, D3DXToRadian(fAngleZ));
-
-	D3DXMATRIX mat = m_matView * matX * matY * matZ;
-	/*D3DXMatrixMultiply(&mat, &m_matView, &matRotation);
-	D3DXMatrixMultiply(&mat, &m_matView, &matRotation);
-	D3DXMatrixMultiply(&mat, &m_matView, &matRotation);*
-
-	MyuAssert( m_pD3dDev->SetTransform(D3DTS_VIEW, &mat), D3D_OK,
-		"CMglCameraMatrixManager::SetCamera()  SetTransform()に失敗" );
-}
-*/
--- mgllib/src/3d/MglCameraMatrixManager.h
+++ mgllib/src/3d/MglCameraMatrixManager.h
@@ -26,31 +26,20 @@ protected:
 	D3DXMATRIX m_matView;
 
 	//	記憶
-	D3DXMATRIX m_matTarget;
-	D3DXMATRIX m_matPos;
 	D3DXVECTOR3 m_vecTarget;
 	D3DXVECTOR3 m_vecPos;
+	D3DXVECTOR3 m_vecRotateTarget;
+	D3DXVECTOR3 m_vecRotatePos;
+	//float m_fRotate; <- m_vecRotatePos.z でいい気がする・・・？
+
+	//	↓いるんかなこれ・・・？（キャッシュとかか・・・？
+	D3DXMATRIX m_matTarget;
+	D3DXMATRIX m_matPos;
 
 	//////////////////////////////////
 
-	float m_fCameraPosX;
-	float m_fCameraPosY;
-	float m_fCameraPosZ;
-	float m_fCameraTargetX;
-	float m_fCameraTargetY;
-	float m_fCameraTargetZ;
-
-	float m_fRotateX;
-	float m_fRotateY;
-	float m_fRotateZ;
-	float m_fMoveX;
-	float m_fMoveY;
-	float m_fMoveZ;
-
-	void CameraLockAt(float fPosX, float fPosY, float fPosZ, float fTargetX, float fTargetY, float fTargetZ, float fRotate);
-
-	//	とりあえず制限に・・・
-	void CameraRotation(int direction, float fAngle);
+	//void CameraLockAt(float fPosX, float fPosY, float fPosZ, float fTargetX, float fTargetY, float fTargetZ, float fRotate);
+	void ReCreateViewMatrix(D3DXVECTOR3 &vecEye, D3DXVECTOR3 &vecAt, D3DXVECTOR3 &vecUp);
 
 public:
 
@@ -72,29 +61,30 @@ public:
 	void ReTransform();
 	
 	//	カメラ
-	void SetCamera(float fPosX, float fPosY, float fPosZ, float fTargetX, float fTargetY, float fTargetZ, float fRotate=0.0f);
+	void SetCamera(float fTargetX, float fTargetY, float fTargetZ, float fPosX, float fPosY, float fPosZ, float fRotate=0.0f){
+		SetCamera( D3DXVECTOR3(fTargetX,fTargetY,fTargetZ), D3DXVECTOR3(fPosX,fPosY,fPosZ), fRotate); }
 	void SetCamera(D3DXMATRIX &matTarget, D3DXMATRIX& matPos, float fRotate=0.0f);
 	void SetCamera(D3DXVECTOR3 &vecTarget, D3DXVECTOR3 &vecPos, float fRotate=0.0f);
 
-	void SetCameraCenter(float x, float y, float z){ SetCameraViewTarget(x,y,z); }
-	void SetCameraViewTarget(float x, float y, float z);
-	void MoveCamera(float x, float y, float z);
-	void SetCameraAngle(float fAngleX, float fAngleY, float fAngleZ);
-	void SetCameraAngle2(float fAngleX, float fAngleY, float fAngleZ);
-	void SetCameraPos(float x, float y, float z);
-	void SetCameraPosition(float x, float y, float z){ SetCameraPos(x,y,z); }
+	void SetTargetPos(float x, float y, float z){ SetTargetPos(D3DXVECTOR3(x,y,z)); }
+	void SetTargetPos(D3DXVECTOR3 &vec);
+	void SetCameraPos(float x, float y, float z){ SetCameraPos(D3DXVECTOR3(x,y,z)); }
+	void SetCameraPos(D3DXVECTOR3 &vec);
+	void MoveTarget(float x, float y, float z){ MoveTarget(D3DXVECTOR3(x,y,z)); }
+	void MoveTarget(D3DXVECTOR3 &vec);
+	void MoveCamera(float x, float y, float z){ MoveCamera(D3DXVECTOR3(x,y,z)); }
+	void MoveCamera(D3DXVECTOR3 &vec);
 
+	void SetCameraAngle(float fAngleX, float fAngleY, float fAngleZ){ SetCameraAngle(D3DXVECTOR3(fAngleX,fAngleY,fAngleZ)); }
+	void SetCameraAngle(D3DXVECTOR3 &vecAngles);
 	void RotateX(float fAngle){ Rotate(fAngle,0,0); }
 	void RotateY(float fAngle){ Rotate(0,fAngle,0); }
 	void RotateZ(float fAngle){ Rotate(0,0,fAngle); }
 	void Rotate(float fAngleX, float fAngleY, float fAngleZ);
 
-	//	各Matrixの設定
-	/*void SetWorldMatrix(D3DXMATRIX &matWorld){ m_matWorld = matWorld; }
-	void SetViewMatrix(D3DXMATRIX &matView){ m_matView = matView; }
-	void SetProjectionMatrix(D3DXMATRIX &matProjection){ m_projection = matProjection; }*/
-	void SetViewMatrix(D3DXMATRIX &matView);
+	/////////////////////////////////////////////////////////
 
+	void SetViewMatrix(D3DXMATRIX &matView);
 	D3DXMATRIX& GetViewMatrix(){ return m_matView; }
 };
 
