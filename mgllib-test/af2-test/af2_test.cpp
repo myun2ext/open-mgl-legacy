3d47e94a1df6eed4ba07c3adf1d85dae380e8ea7
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-04-14 15:36:43 +0000

    af2_test の各ファイルをとりあえず追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@695 6d62ff88-fa28-0410-b5a4-834eb811a934

---
+++ mgllib-test/af2-test/af2_test.cpp
@@ -0,0 +1,17 @@
+// af2_test.cpp : アプリケーション用のエントリ ポイントの定義
+//
+
+#include "stdafx.h"
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow )
+{
+ 	// TODO: この位置にコードを記述してください。
+
+	return 0;
+}
+
+
+
