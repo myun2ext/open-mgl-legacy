2b29ce86f9f300b2ae0b45da3cd89adf782ae7c6
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-04-15 12:58:43 +0000

    ちゃんとdeleteしましょう ってのと OnDropFiles() 追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@714 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib-test/af2-test/af2_test.cpp
+++ mgllib-test/af2-test/af2_test.cpp
@@ -4,7 +4,12 @@
 class CMyWindow : public agh::IWindow
 {
 public:
-//	virtual bool OnClose(){ MessageBox(NULL,"sdfa","sfda",0); return true; }
+	virtual bool OnClose(){ MessageBox(NULL,"sdfa","sfda",0); return true; }
+	virtual bool OnDropFiles(std::vector<std::string> &files){
+		for(int i=0; i<files.size(); i++)
+			MessageBox(NULL,files[i].c_str(),"Drop",0);
+		return true;
+	}
 };
 
 int _MWL_APIENTRY WinMain(
@@ -21,6 +26,8 @@ int _MWL_APIENTRY WinMain(
 	//agh::IWindow* pWindow = fact.CreateWindow(new CMyWindow);
 	agh::IWindow* pWindow = fact.CreateWindow(&myWindow);
 	pWindow->Start();
+
+	delete pWindow;
 
 	return 0;
 }
