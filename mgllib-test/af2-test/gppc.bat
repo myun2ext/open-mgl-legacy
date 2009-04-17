cb5dd5b9e7086c4eca94710af58a8659ebb70b63
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-04-17 02:42:40 +0000

    gppc.bat 追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@733 6d62ff88-fa28-0410-b5a4-834eb811a934

---
+++ mgllib-test/af2-test/gppc.bat
@@ -0,0 +1,3 @@
+g++ af2_test.cpp -I../include/mgl_header -I../AGH/include -I../mwl/mwlagh ../lib/mgllib.lib ../lib/mwlagh.lib
+
+REM g++ af2_test.cpp -I../include/mgl_header -I../AGH/include -I../mwl/mwlagh ../lib/mgllib.exp ../lib/mgllib.lib ../lib/mwlagh.lib
