862b151e925ccb9732097a867f6e55f59ed1c5f3
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-04-17 02:31:19 +0000

    mgllib.lib をプラグマで取り込んじゃうようにしたり等
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@730 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/august/AugustCommon2.h
+++ mgllib/src/august/AugustCommon2.h
@@ -1,6 +1,7 @@
 #ifndef __AugustCommon2_H__
 #define __AugustCommon2_H__
 
+//#define __MWLAGH_NOT_USE_SLIB
 #include "mwlagh.h"
 
 #ifdef MGLLIB_EXPORTS
@@ -13,6 +14,10 @@
 	#define _AGST_DLL_TMPL_EXP /*extern*/ template class _AGST_DLL_EXP	//	C4275‘Î‰ž
 	//#define DLL_TMPL_EXTERN extern
 	//#define DLL_TMPL_EXTERN extern template
+
+	//	2009/04/16 Add.
+	#pragma comment(lib, "mgllib.lib")
+
 #endif
 
 #endif//__AugustCommon2_H__
