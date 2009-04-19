dda6bec829f8b424c259d04dd1ddade9e3f37a04
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-04-19 13:48:06 +0000

    DLL経由（CAugustWindow2）だとどうも上手く動かないので CMwlAghWindow を直接使う形式にしてみたら動いたっぽい。
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@753 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib-test/af2-test/af2_test.cpp
+++ mgllib-test/af2-test/af2_test.cpp
@@ -16,15 +16,27 @@ public:
 	}
 };
 */
+_MWL_HINSTANCE g_hInstance;
 
 //class CMyWindow : public CMwlAghWindow
-class CMyWindow : public CAugustWindow2
+class CMyWindow : public CMwlAghWindow
 {
 public:
 	virtual bool OnClose(){ MessageBox(NULL,"sdfa","sfda",0); return true; }
 	virtual bool OnDropFiles(std::vector<std::string> &files){
 		for(int i=0; i<files.size(); i++)
-			MessageBox(NULL,files[i].c_str(),"Drop",0);
+			MessageBox((HWND)GetHwnd(),files[i].c_str(),"Drop",0);
+
+		RECT rcPos;
+		rcPos.left = 10;
+		rcPos.right = 100;
+		rcPos.top = 50;
+		rcPos.bottom = 80;
+		HWND m_hWnd = ::CreateWindowEx(0, "BUTTON", "Test",
+		0, rcPos.left, rcPos.top, rcPos.right - rcPos.left,
+		rcPos.bottom - rcPos.top, (HWND)GetHwnd(), NULL,
+		g_hInstance, NULL);
+
 		return true;
 	}
 };
@@ -36,6 +48,7 @@ int _MWL_APIENTRY WinMain(
                     int       nCmdShow )
 //int main()
 {
+	g_hInstance = hInstance;
 //::FreeConsole();
 	CMyWindow myWindow;
 	myWindow.EnableDropFiles();
