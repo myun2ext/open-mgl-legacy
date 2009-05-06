1614eac2401834ba3c638e8b1a82d102be6001d0
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-05-06 15:52:35 +0000

    VC++2008用プロジェクトファイル追加とVC++2008対応
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@833 6d62ff88-fa28-0410-b5a4-834eb811a934

--- _package_test/test/StdAfx.cpp
+++ _package_test/test/StdAfx.cpp
@@ -2,6 +2,7 @@
 //              test.pch 生成されるプリコンパイル済ヘッダー
 //              stdafx.obj 生成されるプリコンパイル済タイプ情報
 
+//#define DWORD_PTR DWORD*
 #include "stdafx.h"
 
 // TODO: STDAFX.H に含まれていて、このファイルに記述されていない
--- _package_test/test/test_081201b.cpp
+++ _package_test/test/test_081201b.cpp
@@ -2,43 +2,52 @@
 //
 
 #include "stdafx.h"
-#include "MglOgg.h"
+//#include "MglOgg.h"
 
 class CMglTestFrame : public CMglguiWindow
 {
 private:
 	CMglAghImage m_img;
-	CMglOgg m_music;
+	CMglAghText m_text;
+	CMglBgm m_music;
 public:
 	//	初期化時に呼ばれる
 	void OnInit(){
 		EnableEscEnd();
 		m_img.Load("test.jpg");
 		CMglguiWindow::RegistControl(&m_img);
+		CMglguiWindow::RegistControl(&m_text);
+
+		m_text.SetText("ほげげほげほ");
+		m_text.SetFontSize(150);
+		m_text.SetColor( AGHCOLOR_ARGB(120,180,180,255) );
+		m_text.SetPos(80,380);
+
+		m_img.SetColor(0xf0ffffff);
 
 		//	キーボードイベントハンドラ登録（左）- 押している間移動
 		RegistKbHandler(
 			MGL_KB_EVT_HANDLER_EVTTYPE_ON_PRESS,
 			DIK_LEFT,
-			(MGL_KB_EVT_HANDLER_CALLBACK)MoveLeft);
+			(MGL_KB_EVT_HANDLER_CALLBACK)&CMglTestFrame::MoveLeft);
 
 		//	キーボードイベントハンドラ登録（右）- 押している間移動
 		RegistKbHandler(
 			MGL_KB_EVT_HANDLER_EVTTYPE_ON_PRESS,
 			DIK_RIGHT,
-			(MGL_KB_EVT_HANDLER_CALLBACK)MoveRight);
+			(MGL_KB_EVT_HANDLER_CALLBACK)&CMglTestFrame::MoveRight);
 
 		//	キーボードイベントハンドラ登録（上）- 離した瞬間だけ移動
 		RegistKbHandler(
 			MGL_KB_EVT_HANDLER_EVTTYPE_ON_KEYUP,
 			DIK_UP,
-			(MGL_KB_EVT_HANDLER_CALLBACK)MoveUp);
+			(MGL_KB_EVT_HANDLER_CALLBACK)&CMglTestFrame::MoveUp);
 
 		//	キーボードイベントハンドラ登録（下）- 押した瞬間だけ移動
 		RegistKbHandler(
 			MGL_KB_EVT_HANDLER_EVTTYPE_ON_KEYDOWN,
 			DIK_DOWN,
-			(MGL_KB_EVT_HANDLER_CALLBACK)MoveDown);
+			(MGL_KB_EVT_HANDLER_CALLBACK)&CMglTestFrame::MoveDown);
 
 
 		m_audio.Load("pwr_snr2.wav","a");
@@ -46,9 +55,10 @@ public:
 		RegistKbHandler(
 			MGL_KB_EVT_HANDLER_EVTTYPE_ON_KEYDOWN,
 			DIK_Z,
-			(MGL_KB_EVT_HANDLER_CALLBACK)PlaySoundZ);
+			(MGL_KB_EVT_HANDLER_CALLBACK)&CMglTestFrame::PlaySoundZ);
 
-		m_music.Load("dante.ogg");
+		//m_music.Load("dante.ogg");
+		m_music.Load("kanon_lstrgt_org.mp3");
 		m_music.Play();
 	}
 	bool MoveLeft(){
@@ -75,7 +85,7 @@ public:
 	void OnCreateWindow(agh::CREATE_WINDOW_INFO *pWindowInfo){
 		pWindowInfo->nWinWidthSize = 800;
 		pWindowInfo->nWinHeightSize = 600;
-		pWindowInfo->strWindowTitle = "さんぷるぷろぐらむ";
+		//pWindowInfo->strWindowTitle = "さんぷるぷろぐらむ";
 	}
 };
 
