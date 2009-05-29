b6281f8cd0c5fdc3eb550853f3860930d06a63d6
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-05-29 18:51:43 +0000

    とりあえず追加（AugustKeyboardInput, AugustMouseInput）
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@892 6d62ff88-fa28-0410-b5a4-834eb811a934

---
+++ mgllib/src/august/AugustKeyboardInput.cpp
@@ -0,0 +1,156 @@
+//////////////////////////////////////////////////////////
+//
+//	AugustKeyboardInput
+//		- MglGraphicManager レイヤークラス
+//
+//////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "AugustKeyboardInput.h"
+
+using namespace agh;
+using namespace std;
+
+class DLL_EXP agh::CScreenBase;
+
+/*
+//	コンストラクタ
+CAugustKeyboardInput::CAugustKeyboardInput()
+{
+}
+
+//	デストラクタ
+CAugustKeyboardInput::~CAugustKeyboardInput()
+{
+}
+*/
+
+
+//////////////////////////////////////////////////
+
+void CAugustKeyboardInput::OnDraw()
+{
+	g_->pGrp->Clear(m_rgbBackground);	//	2008/11/29 CAugustKeyboardInput対応
+
+	//for(int i=0; i<m_ctrlPtrAry.size(); i++)
+	for(int i=0; i<m_ctrlPtrAry.size(); _vcpp(i))
+	{
+		//m_ctrlPtrAry[i]->Draw();
+		GetVCtrlPtr(i)->Draw();
+	}
+}
+
+
+//	フレーム処理
+bool CAugustKeyboardInput::DoFrame()
+{
+	//	2009/01/23 マウスのハンドルは無効に出来るように。
+	//	（結構処理長いし、うっかりクリックとかで余計な処理走るとまずいし・・・）
+	if ( m_bUseMouseHandle == true )
+	{
+		OnFrameMouseInput();	//	なんか拾わないとfalseを返す仕様になってるっぽい・・・
+		/*if ( OnFrameMouseInput() != true )
+			return false;*/
+	}
+
+	//	キーボードのハンドル処理
+	if ( OnFrameKeyboardInput() != true )
+		return false;
+
+	//	2008/11/02 ユーザイベント処理
+	if ( OnFrameDoUser() != true )
+		return false;
+
+	//	各コントロールのフレーム処理
+	//for(int i=0; i<m_ctrlPtrAry.size(); i++)
+	//for(int i=0; i<m_ctrlPtrAry.size(); _vcpp(i))
+	for(citr it=cbegin(); it != cend(); it++)
+	//for(vector<CControlBase*>::iterator it=cbegin(); it != cend(); it++)
+	{
+		it->OnFrame();
+		//(*it)->OnFrame();
+		//((CControlBase*)it)->OnFrame();
+		//m_ctrlPtrAry[i]->OnFrame();
+		//GetVCtrlPtr(i)->Draw();
+	}
+
+	return true;
+}
+
+
+/////////////////////////////////////////////////////////////
+
+//	Eventsへ。
+
+/////////////////////////////////////////////////////////////
+
+/*
+void CAugustKeyboardInput::RegistControl(CMglAghImage* pImage)
+{
+	const char* szFilePath = pImage->m_strFilePath.c_str();
+	const char* szAlias = pImage->m_strLayerName.c_str();
+
+	//	せっとあっぷ
+	pImage->_Setup(g_->pGrp, g_->pImgCache);
+	
+	//	きゃっしゅ
+	g_->pImgCache->Cache(szFilePath);
+
+	pImage->SetRect(0,0,g_->pImgCache->Get(szFilePath)->GetBmpWidth(),
+		g_->pImgCache->Get(szFilePath)->GetBmpHeight());
+
+	//	そして登録
+	_RegistControl(pImage);
+	//m_layer.RegistBegin( new CMglImageLayer(m_imgCache[szFilePath]), true );
+}
+*/
+
+//	RegistControl: CAugustVisualControlBase
+void CAugustKeyboardInput::RegistControl(CAugustVisualControlBase* pControl)
+{
+	pControl->OnRegist(g_);
+
+	_RegistControlInternal(pControl);
+}
+
+//	RegistControl: CAugustVisualControlBase2
+void CAugustKeyboardInput::RegistControl(CAugustVisualControlBase2* pControl)
+{
+	pControl->OnRegist(g_);
+
+	_RegistControlInternal(pControl->_GetAghControl());
+}
+
+//	RegistControl: CControlBase
+/*void CAugustKeyboardInput::RegistControl(CControlBase* pControl)
+{
+	pControl->SetParentControl(this);
+
+	_RegistControlInternal(pControl);
+}*/
+
+//	RegistSubControl: CAugustCtrlBase
+void CAugustKeyboardInput::RegistSubControl(CAugustCtrlBase* pControl)
+{
+	CControlBase::RegistSubControl(pControl->_GetAghControl());
+	pControl->_SetImplRoot(this->g_);
+
+	/*
+	pControl->_GetAghControl()->SetParentControl(this);
+	pControl->_SetImplRoot(this->g_);
+
+	_RegistControlInternal(pControl->_GetAghControl());
+	*/
+}
+
+/*void CAugustKeyboardInput::RegistControl(agh::CControlBase* pControl)
+{
+	pControl->OnRegist(g_);
+
+	_RegistControlInternal(pControl);
+}*/
+
+void CAugustKeyboardInput::_RegistControlInternal(agh::CControlBase* pCtrl)
+{
+	m_ctrlPtrAry.push_back(pCtrl);
+}
---
+++ mgllib/src/august/AugustKeyboardInput.h
@@ -0,0 +1,233 @@
+//////////////////////////////////////////////////////////
+//
+//	AugustKeyboardInput
+//
+//////////////////////////////////////////////////////////
+
+#ifndef __AugustKeyboardInput_H__
+#define __AugustKeyboardInput_H__
+
+#include "agh.h"
+#include "MglImageCacher.h"
+#include "MglImageLayer.h"
+#include "MglInput.h"
+#include "MglAudio.h"
+#include "MglAghImage.h"
+#include "AugustCommon.h"
+//#include "MglMouseInput.h"
+
+class DLL_EXP agh::CScreenBase;
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+//	キーボードイベントハンドラ系
+
+#define AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_PRESS		(1)	//	押されている間
+#define AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_KEYDOWN	(2)	//	キーボードが押された瞬間
+#define AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_KEYUP		(3)	//	キーボードが離された瞬間
+/*#define MGL_KB_EVT_HANDLER_EVTTYPE_ON_DOWNUP	(8)	//	押した後話した（クリック的な）
+#define MGL_KB_EVT_HANDLER_EVTTYPE_ON_PUSH		(MGL_KB_EVT_HANDLER_EVTTYPE_ON_DOWNUP)*/
+
+//	キーボードイベントハンドラ構造体
+class CAugustKeyboardInput;
+typedef bool (CAugustKeyboardInput::*AUGUST_KB_EVT_HANDLER_CALLBACK)();
+typedef struct {
+	AUGUST_KB_EVT_HANDLER_CALLBACK pCallbackFunc;
+	BYTE keyCode;
+	short evtType;
+} AUGUST_KB_EVT_HANDLER;
+typedef list<AUGUST_KB_EVT_HANDLER> t_AUGUST_KB_EVT_HANDLERS;
+
+/////////////////////////////////////////////////////////////////////////
+
+//	クラス宣言  /////////////////////////////////////////////////////////
+class DLL_EXP CAugustKeyboardInput : public agh::CScreenBase
+{
+protected:
+	CAugustGlobalCommon *g_;
+	//CMglGraphicManager m_grp; <- 間違いでは・・・？
+	/*CMglGraphicManager *m_grp;	//	Alias
+	CMglInput *m_input;			//	Alias
+	CMglMouseInput *m_mouse;	//	Alias
+	CMglAudio *m_audio;			//	Alias*/
+
+	//	2008/11/26 Add. デフォルトのイメージ配列
+	//map<std::string,CAugustImage> m_imgAry;
+
+	//	2008/11/26 Add. デフォルトのイメージ配列
+	//list<bool (*)()> m_kbEventHandlers;	//	本当はvector_list使うネ・・・
+	//list<MGL_KB_EVT_HANDLER_CALLBACK> m_kbEventHandlers;	//	本当はvector_list使うネ・・・
+	list<AUGUST_KB_EVT_HANDLER> m_kbEventHandlers;	//	本当はvector_list使うネ・・・
+
+	/////////////////////////////////////////////////////////
+
+	//HWND m_hWnd;
+	//CMglLayers4 m_layer;
+	//CMglImageCacher m_imgCache;
+	//CMglImageCacher &m_imgCache;
+	D3DCOLOR m_rgbBackground;
+	POINT m_nCachedCurPos;
+	int m_nCachedCurPosX;
+	int m_nCachedCurPosY;
+	bool m_bUseMouseHandle;
+
+	agh::_AGH_POINT GetCurPos(){
+		return *((agh::_AGH_POINT*)&m_nCachedCurPos);
+	}
+
+_AGH_EVENT_ACCESS_MODIFIER:
+	///// オーバーライド可能なイベント /////////////////////////////////////////////////
+
+	/**
+	  ここにあった OnControl, OnBackground は下に移動しといたよ。
+	**/
+
+	virtual void OnDraw();
+	virtual bool DoFrame();
+
+	//	このクラスから
+	virtual bool OnFrameDoUser(){return true;}
+	virtual bool OnFrameKeyboardInput();
+	virtual bool OnFrameMouseInput();
+
+protected:
+
+private:
+	//	なんでPublic？（Privateではないのか・・・？）
+	//void OnLButtonDown(int x, int y);
+
+	//void ScreenUpdate();
+
+	void _RegistControlInternal(agh::CControlBase* pCtrl);// <- なんのためにあるの・・・？旧時代の遺物か？ 
+
+public:
+	//	コンストラクタ
+	/*CAugustKeyboardInput(CAugustGlobalCommon &g_in) : g_(g_in),
+		m_mouse(g_in.input.mouse), m_grp(g_in.grp), m_input(g_in.input), m_audio(g_in.audio),
+		m_imgCache(g_in.imgCache)
+	{
+		//m_hWnd = NULL;
+		m_rgbBackground = D3DCOLOR_WHITE;
+	}*/
+	//	コンストラクタ
+	CAugustKeyboardInput()
+	{
+		//m_hWnd = NULL;
+		m_rgbBackground = D3DCOLOR_WHITE;
+		g_ = NULL;
+		ZeroMemory(&m_nCachedCurPos, sizeof(m_nCachedCurPos));
+		m_nCachedCurPosX = -1;
+		m_nCachedCurPosY = -1;
+		m_bUseMouseHandle = false;
+	}
+	virtual ~CAugustKeyboardInput(){}
+	void Setup(CAugustGlobalCommon *g_in){
+		g_ = g_in;
+	}
+
+	///// コントロールの登録 /////////////////////////////////////////////////
+
+	//void RegistControl(CMglAghImage* pImage);
+	void RegistControl(CAugustVisualControlBase* pControl);
+	void RegistControl(CAugustVisualControlBase2* pControl);
+	//void RegistControl(agh::CControlBase* pControl);
+	void RegistSubControl(CAugustCtrlBase* pControl);
+
+	//////////////////////////////////////////////////////
+
+	//BOOL IsExistPool(const char* szAlias); <- ?
+
+	/*BOOL InsertImage(IMGLIST_ITR it);
+	BOOL InsertImage(){ return InsertImage(GetScene()->m_images.begin()); }*/
+
+	void SetBackgroundColor(D3DCOLOR color){ m_rgbBackground = color; }
+
+	//	キーボードハンドラの登録
+	void RegistKbHandler(AUGUST_KB_EVT_HANDLER &evt){
+		m_kbEventHandlers.push_back(evt);
+	}
+	void RegistKbHandler(
+		short evtType,
+		BYTE keyCode,		
+		AUGUST_KB_EVT_HANDLER_CALLBACK pCallbackFunc)
+	{
+		AUGUST_KB_EVT_HANDLER evt;
+		evt.pCallbackFunc = pCallbackFunc;
+		evt.keyCode = keyCode;
+		evt.evtType = evtType;
+		m_kbEventHandlers.push_back(evt);
+	}
+
+	void EnableMouseHandle(){ m_bUseMouseHandle = true; }
+	void DisableMouseHandle(){ m_bUseMouseHandle = false; }
+
+public:
+	//bool OnFrameMouseInput(); <- なんかpublicなのに理由あんのかな・・・？
+};
+
+
+
+	/*virtual void OnBackgroundLButtonDown(int x, int y){}
+	virtual void OnBackgroundRButtonDown(int x, int y){}
+	virtual void OnBackgroundCButtonDown(int x, int y){}
+	virtual void OnControlLButtonDown(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnControlRButtonDown(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnControlCButtonDown(agh::CControlBase *pControl, int x, int y){}
+
+	//	左クリック系 : Left Click Events
+	virtual void OnLButtonClick(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnLButtonDblClk(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnLButtonDown(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnLButtonUp(agh::CControlBase *pControl, int x, int y){}
+
+	//	右クリック系 : Right Click Events
+	virtual void OnRButtonClick(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnRButtonDblClk(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnRButtonDown(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnRButtonUp(agh::CControlBase *pControl, int x, int y){}
+
+	//	マウス移動 : Move Mouse Events
+	virtual void OnMouseMove(agh::CControlBase *pControl, int nPosX, int nPosY, int nMoveX, int nMoveY){}
+
+	//	ホイール : Wheel Events
+	virtual void OnWheelUp(agh::CControlBase *pControl, int count){}
+	virtual void OnWheelDown(agh::CControlBase *pControl, int count){}
+	virtual void OnCButtonClick(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnCButtonDblClk(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnCButtonDown(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnCButtonUp(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnMButtonClick(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnMButtonDblClk(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnMButtonDown(agh::CControlBase *pControl, int x, int y){}
+	virtual void OnMButtonUp(agh::CControlBase *pControl, int x, int y){}*/
+
+	/*
+	//	左クリック系 : Left Click Events
+	virtual void OnBackgroundLButtonClick(int x, int y){}
+	virtual void OnBackgroundLButtonDblClk(int x, int y){}
+	virtual void OnBackgroundLButtonDown(int x, int y){}
+	virtual void OnBackgroundLButtonUp(int x, int y){}
+
+	//	右クリック系 : Right Click Events
+	virtual void OnBackgroundRButtonClick(int x, int y){}
+	virtual void OnBackgroundRButtonDblClk(int x, int y){}
+	virtual void OnBackgroundRButtonDown(int x, int y){}
+	virtual void OnBackgroundRButtonUp(int x, int y){}
+
+	//	マウス移動 : Move Mouse Events
+	virtual void OnBackgroundMouseMove(int nPosX, int nPosY, int nMoveX, int nMoveY){}
+
+	//	ホイール : Wheel Events
+	virtual void OnBackgroundWheelUp(int count){}
+	virtual void OnBackgroundWheelDown(int count){}
+	virtual void OnBackgroundCButtonClick(int x, int y){}
+	virtual void OnBackgroundCButtonDblClk(int x, int y){}
+	virtual void OnBackgroundCButtonDown(int x, int y){}
+	virtual void OnBackgroundCButtonUp(int x, int y){}
+	virtual void OnBackgroundMButtonClick(int x, int y){}
+	virtual void OnBackgroundMButtonDblClk(int x, int y){}
+	virtual void OnBackgroundMButtonDown(int x, int y){}
+	virtual void OnBackgroundMButtonUp(int x, int y){}
+	*/
+
+#endif//__AugustKeyboardInput_H__
