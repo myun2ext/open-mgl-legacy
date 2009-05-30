e6d1e9486991e2fa337aaa0d1e499a0c4cf9aaf3
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-05-30 17:47:11 +0000

    CAugustKeyboardInput::OnFrame() をとりあえず実装
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@895 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/august/AugustKeyboardInput.cpp
+++ mgllib/src/august/AugustKeyboardInput.cpp
@@ -27,3 +27,36 @@ CAugustKeyboardInput::~CAugustKeyboardInput()
 
 
 //////////////////////////////////////////////////
+
+bool CAugustKeyboardInput::OnFrame()
+{
+	t_AUGUST_KB_EVT_HANDLERS::iterator it=m_kbEventHandlers.begin();
+	for(; it!=m_kbEventHandlers.end(); it++)
+	{
+		AUGUST_KB_EVT_HANDLER &r = *it;
+
+		switch(r.evtType)
+		{
+		case AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_PRESS:
+			if ( g_->pInput->IsPressKey(r.keyCode) )
+				if ( (this->*r.pCallbackFunc)() != true )
+					return false;
+			break;
+
+		case AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_KEYDOWN:
+			if ( g_->pInput->IsOnDownKey(r.keyCode) )
+				if ( (this->*r.pCallbackFunc)() != true )
+					return false;
+			break;
+
+		case AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_KEYUP:
+			if ( g_->pInput->IsOnUpKey(r.keyCode) )
+				if ( (this->*r.pCallbackFunc)() != true )
+					return false;
+			break;
+		}
+	}
+
+	return true;
+}
+
