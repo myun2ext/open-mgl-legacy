c4cd7752527379d3283077d8ccdd64d7f6617f96
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-07-17 16:03:08 +0000

    ここらへんのメソッドやら何やらは軒並み要らなさそう
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@961 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/august/AugustKeyboardInput.h
+++ mgllib/src/august/AugustKeyboardInput.h
@@ -9,39 +9,13 @@
 
 #include "agh.h"
 
-//////////////////////////////////////////////////////////////////////////////////////
-
-//	キーボードイベントハンドラ系
-
-#define AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_PRESS		(1)	//	押されている間
-#define AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_KEYDOWN	(2)	//	キーボードが押された瞬間
-#define AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_KEYUP		(3)	//	キーボードが離された瞬間
-/*#define MGL_KB_EVT_HANDLER_EVTTYPE_ON_DOWNUP	(8)	//	押した後話した（クリック的な）
-#define MGL_KB_EVT_HANDLER_EVTTYPE_ON_PUSH		(MGL_KB_EVT_HANDLER_EVTTYPE_ON_DOWNUP)*/
-
-//	キーボードイベントハンドラ構造体
-class CAugustKeyboardInput;
-typedef bool (CAugustKeyboardInput::*AUGUST_KB_EVT_HANDLER_CALLBACK)();
-typedef struct {
-	AUGUST_KB_EVT_HANDLER_CALLBACK pCallbackFunc;
-	BYTE keyCode;
-	short evtType;
-} AUGUST_KB_EVT_HANDLER;
-typedef list<AUGUST_KB_EVT_HANDLER> t_AUGUST_KB_EVT_HANDLERS;
-
-class CMglKeyboardInput;
-
-/////////////////////////////////////////////////////////////////////////
+class CMglInput;
 
 //	クラス宣言  /////////////////////////////////////////////////////////
-class DLL_EXP CAugustKeyboardInput : public agh::CControlBase
+class DLL_EXP CAugustKeyboardInput : public agh::CKeyboardBase
 {
 protected:
 	CMglInput *m_pInput;			//	Alias
-
-	//list<bool (*)()> m_kbEventHandlers;	//	本当はvector_list使うネ・・・
-	//list<MGL_KB_EVT_HANDLER_CALLBACK> m_kbEventHandlers;	//	本当はvector_list使うネ・・・
-	list<AUGUST_KB_EVT_HANDLER> m_kbEventHandlers;	//	本当はvector_list使うネ・・・
 
 	/////////////////////////////////////////////////////////
 
@@ -61,21 +35,6 @@ public:
 
 	//////////////////////////////////////////////////////
 
-	//	キーボードハンドラの登録
-	void RegistKbHandler(AUGUST_KB_EVT_HANDLER &evt){
-		m_kbEventHandlers.push_back(evt);
-	}
-	void RegistKbHandler(
-		short evtType,
-		BYTE keyCode,		
-		AUGUST_KB_EVT_HANDLER_CALLBACK pCallbackFunc)
-	{
-		AUGUST_KB_EVT_HANDLER evt;
-		evt.pCallbackFunc = pCallbackFunc;
-		evt.keyCode = keyCode;
-		evt.evtType = evtType;
-		m_kbEventHandlers.push_back(evt);
-	}
 };
 
 
