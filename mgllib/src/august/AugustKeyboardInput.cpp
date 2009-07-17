86479f66da46d80b47b30f979b821932a1bb7fbc
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-07-17 16:25:36 +0000

    あー違う違う。こうだこうだ。
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@962 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/august/AugustKeyboardInput.cpp
+++ mgllib/src/august/AugustKeyboardInput.cpp
@@ -13,17 +13,46 @@ using namespace std;
 
 class DLL_EXP agh::CControlBase;
 
-/*
+
+//	コアの基底クラス
+class CAugustKeyboardCore : public agh::CKeyboardCoreBase
+{
+public:
+	virtual bool IsOnKeyEvt(agh::KB_EVTTYPE_t evtType, agh::KEYCODE_t keyCode)
+	{
+		switch(evtType)
+		{
+		//	押されている間
+		case AGH_KB_EVT_HANDLER_EVTTYPE_ON_PRESS:
+			break;
+
+		//	キーボードが押された瞬間
+		case AGH_KB_EVT_HANDLER_EVTTYPE_ON_KEYDOWN:
+			break;
+
+		//	キーボードが話された瞬間
+		case AGH_KB_EVT_HANDLER_EVTTYPE_ON_KEYUP:
+			break;
+		}
+
+		return false;
+	}
+};
+
+///////////////////////////////////////////////////////////////////////////
+
 //	コンストラクタ
 CAugustKeyboardInput::CAugustKeyboardInput()
 {
+	m_pCore = new CAugustKeyboardCore();
 }
 
 //	デストラクタ
 CAugustKeyboardInput::~CAugustKeyboardInput()
 {
+	delete m_pCore;
 }
-*/
+
 
 
 
--- mgllib/src/august/AugustKeyboardInput.h
+++ mgllib/src/august/AugustKeyboardInput.h
@@ -9,32 +9,14 @@
 
 #include "agh.h"
 
-class CMglInput;
-
 //	クラス宣言  /////////////////////////////////////////////////////////
+
 class DLL_EXP CAugustKeyboardInput : public agh::CKeyboardBase
 {
-protected:
-	CMglInput *m_pInput;			//	Alias
-
-	/////////////////////////////////////////////////////////
-
-_AGH_EVENT_ACCESS_MODIFIER:
-	///// オーバーライド可能なイベント /////////////////////////////////////////////////
-
-	/**
-	  ここにあった OnControl, OnBackground は下に移動しといたよ。
-	**/
-	virtual bool DoFrame();
-
 public:
-	//	コンストラクタ
-	//	コンストラクタ
-	CAugustKeyboardInput(){}
-	virtual ~CAugustKeyboardInput(){}
-
-	//////////////////////////////////////////////////////
-
+	//	コンストラクタ・デストラクタ
+	CAugustKeyboardInput();
+	virtual ~CAugustKeyboardInput();
 };
 
 
