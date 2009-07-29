f05a7ffd2eda60320a92cee8350ff7d885922b1a
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-07-29 15:07:37 +0000

    CAugustMouseInput: イチから作り直し。とりあえずKeyboardの奴からコピーして名前置換＋αした程度
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@976 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib/src/august/AugustMouseInput.cpp
+++ mgllib/src/august/AugustMouseInput.cpp
@@ -7,171 +7,276 @@
 
 #include "stdafx.h"
 #include "AugustMouseInput.h"
+#include "MglInput.h"
 
 using namespace agh;
 using namespace std;
 
-class DLL_EXP agh::CScreenBase;
+class DLL_EXP agh::CControlBase;
 
-/*
+///////////////////////////////////////////////////////////////
+
+#define AUGUST_KEYCODE_TABLE_SIZE	(256)
+
+//#define PRESSKEY(DIK_CODE)	(DIK_CODE & 0x80)
+
+//	コアの基底クラス
+class CAugustMouseCore : public agh::CMouseCoreBase
+{
+protected:
+	CMglMouseInput m_input;
+
+	static int m_keyCodeTable[AUGUST_KEYCODE_TABLE_SIZE];
+	static bool m_bKeyCodeTableInited;
+
+private:
+	void GenKcTable();
+
+public:
+	//	コンストラクタ・デストラクタ
+	CAugustMouseCore(){ GenKcTable(); }
+	virtual ~CAugustMouseCore(){}
+
+	/* override */
+	//	多分trueで帰ることにより処理続行？
+	//	2009/07/28  ぎゃくぎゃく
+	virtual bool IsOnKeyEvt(agh::KB_EVTTYPE_t evtType, agh::KEYCODE_t keyCode)
+	{
+		BYTE dik = GetAghKeyToDik(keyCode);
+		if ( dik == 0 )
+			return false;
+			//return true;	2009/07/28  ぎゃくぎゃく
+
+		BOOL isOnEvt = FALSE;
+		switch(evtType)
+		{
+		//	押されている間
+		case AGH_KB_EVT_HANDLER_EVTTYPE_ON_PRESS:
+			isOnEvt = m_input.IsPressKey(dik);
+			break;
+
+		//	キーボードが押された瞬間
+		case AGH_KB_EVT_HANDLER_EVTTYPE_ON_KEYDOWN:
+			isOnEvt = m_input.IsOnDownKey(dik);
+			break;
+
+		//	キーボードが話された瞬間
+		case AGH_KB_EVT_HANDLER_EVTTYPE_ON_KEYUP:
+			isOnEvt = m_input.IsOnUpKey(dik);
+			break;
+		}
+
+		if ( isOnEvt )
+			return true;
+		else
+			return false;
+		//return true;	2009/07/28  ぎゃくぎゃく
+	}
+
+	//	初期化
+	void Init(HWND hWnd)
+	{
+		m_input.Init(hWnd);
+	}
+
+	static BYTE GetAghKeyToDik(KEYCODE_t aghKey){ return m_keyCodeTable[aghKey]; }	//	255以上の値で来た場合の動作は未定義
+};
+
+////////////////////////////////////////////////////////////////////////
+
+//	staticな変数の実体
+int CAugustMouseCore::m_keyCodeTable[AUGUST_KEYCODE_TABLE_SIZE];
+bool CAugustMouseCore::m_bKeyCodeTableInited = false;
+
+////////////////////////////////////////////////////////////////////////
+
+//	テーブルの生成
+void CAugustMouseCore::GenKcTable()
+{
+	if ( m_bKeyCodeTableInited == true )
+		return;
+
+	ZeroMemory( m_keyCodeTable, sizeof(m_keyCodeTable) );
+
+	//	配列の生成
+	m_keyCodeTable['0'] = DIK_0;
+	m_keyCodeTable['1'] = DIK_1;
+	m_keyCodeTable['2'] = DIK_2;
+	m_keyCodeTable['3'] = DIK_3;
+	m_keyCodeTable['4'] = DIK_4;
+	m_keyCodeTable['5'] = DIK_5;
+	m_keyCodeTable['6'] = DIK_6;
+	m_keyCodeTable['7'] = DIK_7;
+	m_keyCodeTable['8'] = DIK_8;
+	m_keyCodeTable['9'] = DIK_9;
+
+	m_keyCodeTable['A'] = DIK_A;
+	m_keyCodeTable['B'] = DIK_B;
+	m_keyCodeTable['C'] = DIK_C;
+	m_keyCodeTable['D'] = DIK_D;
+	m_keyCodeTable['E'] = DIK_E;
+	m_keyCodeTable['F'] = DIK_F;
+	m_keyCodeTable['G'] = DIK_G;
+	m_keyCodeTable['H'] = DIK_H;
+	m_keyCodeTable['I'] = DIK_I;
+	m_keyCodeTable['J'] = DIK_J;
+	m_keyCodeTable['K'] = DIK_K;
+	m_keyCodeTable['L'] = DIK_L;
+	m_keyCodeTable['M'] = DIK_M;
+	m_keyCodeTable['N'] = DIK_N;
+	m_keyCodeTable['O'] = DIK_O;
+	m_keyCodeTable['P'] = DIK_P;
+	m_keyCodeTable['Q'] = DIK_Q;
+	m_keyCodeTable['R'] = DIK_R;
+	m_keyCodeTable['S'] = DIK_S;
+	m_keyCodeTable['T'] = DIK_T;
+	m_keyCodeTable['U'] = DIK_U;
+	m_keyCodeTable['V'] = DIK_V;
+	m_keyCodeTable['W'] = DIK_W;
+	m_keyCodeTable['X'] = DIK_X;
+	m_keyCodeTable['Y'] = DIK_Y;
+	m_keyCodeTable['Z'] = DIK_Z;
+
+	//	記号系  2007/01/02
+	m_keyCodeTable['-'] = DIK_MINUS;
+	m_keyCodeTable[','] = DIK_COMMA;
+	m_keyCodeTable['.'] = DIK_PERIOD;
+	m_keyCodeTable['/'] = DIK_SLASH;
+	m_keyCodeTable[';'] = DIK_SEMICOLON;
+	m_keyCodeTable[':'] = DIK_COLON;
+	m_keyCodeTable['@'] = DIK_AT;
+	m_keyCodeTable['['] = DIK_LBRACKET;
+	m_keyCodeTable[']'] = DIK_RBRACKET;
+	m_keyCodeTable['\\'] = DIK_YEN;
+	m_keyCodeTable['^'] = DIK_CIRCUMFLEX;
+	//m_keyCodeTable['?'] = DIK_SLASH;	//	ダミー
+
+	m_keyCodeTable['a'] = DIK_A;
+	m_keyCodeTable['b'] = DIK_B;
+	m_keyCodeTable['c'] = DIK_C;
+	m_keyCodeTable['d'] = DIK_D;
+	m_keyCodeTable['e'] = DIK_E;
+	m_keyCodeTable['f'] = DIK_F;
+	m_keyCodeTable['g'] = DIK_G;
+	m_keyCodeTable['h'] = DIK_H;
+	m_keyCodeTable['i'] = DIK_I;
+	m_keyCodeTable['j'] = DIK_J;
+	m_keyCodeTable['k'] = DIK_K;
+	m_keyCodeTable['l'] = DIK_L;
+	m_keyCodeTable['m'] = DIK_M;
+	m_keyCodeTable['n'] = DIK_N;
+	m_keyCodeTable['o'] = DIK_O;
+	m_keyCodeTable['p'] = DIK_P;
+	m_keyCodeTable['q'] = DIK_Q;
+	m_keyCodeTable['r'] = DIK_R;
+	m_keyCodeTable['s'] = DIK_S;
+	m_keyCodeTable['t'] = DIK_T;
+	m_keyCodeTable['u'] = DIK_U;
+	m_keyCodeTable['v'] = DIK_V;
+	m_keyCodeTable['w'] = DIK_W;
+	m_keyCodeTable['x'] = DIK_X;
+	m_keyCodeTable['y'] = DIK_Y;
+	m_keyCodeTable['z'] = DIK_Z;
+
+	//	小文字はどうする・・・？
+	/*
+	if ( m_bCaptialReverse )
+	{
+		m_keyCodeTable['a'] = DIK_A;
+		m_keyCodeTable['b'] = DIK_B;
+		m_keyCodeTable['c'] = DIK_C;
+		m_keyCodeTable['d'] = DIK_D;
+		m_keyCodeTable['e'] = DIK_E;
+		m_keyCodeTable['f'] = DIK_F;
+		m_keyCodeTable['g'] = DIK_G;
+			:
+			:
+	*/
+
+	//	GetOnKeyList 用の追加リスト 2007/01/01
+	m_keyCodeTable[ASCII_BS] =		DIK_BACKSPACE;
+	//m_keyCodeTable[ASCII_TAB] =	DIK_TAB;
+	m_keyCodeTable['\t'] =			DIK_TAB;
+	m_keyCodeTable[ASCII_ENTER] =	DIK_RETURN;
+	m_keyCodeTable[ASCII_ESC] =		DIK_ESCAPE;
+	//m_keyCodeTable[ASCII_SP] =	DIK_SPACE;
+	m_keyCodeTable[' '] =			DIK_SPACE;
+	m_keyCodeTable[ASCII_DELETE] =	DIK_DELETE;
+
+	/* ######################################################## */
+	/*                     */
+	/*   AGH 固有のコード  */
+	/*                     */
+	/* ######################################################## */
+
+
+	m_keyCodeTable[AGH_KEYCODE_UP]		=	DIK_UP;
+	m_keyCodeTable[AGH_KEYCODE_DOWN]	=	DIK_DOWN;
+	m_keyCodeTable[AGH_KEYCODE_LEFT]	=	DIK_LEFT;
+	m_keyCodeTable[AGH_KEYCODE_RIGHT]	=	DIK_RIGHT;
+
+	m_keyCodeTable[AGH_KEYCODE_LCTRL]	=	DIK_LCONTROL;
+	m_keyCodeTable[AGH_KEYCODE_RCTRL]	=	DIK_RCONTROL;
+	m_keyCodeTable[AGH_KEYCODE_LSHIFT]	=	DIK_LSHIFT;
+	m_keyCodeTable[AGH_KEYCODE_RSHIFT]	=	DIK_RSHIFT;
+	m_keyCodeTable[AGH_KEYCODE_LALT]	=	DIK_LALT;
+	m_keyCodeTable[AGH_KEYCODE_RALT]	=	DIK_RALT;
+	m_keyCodeTable[AGH_KEYCODE_LWIN]	=	DIK_LWIN;
+	m_keyCodeTable[AGH_KEYCODE_RWIN]	=	DIK_RWIN;
+	m_keyCodeTable[AGH_KEYCODE_MENU]	=	DIK_RMENU;
+
+	m_keyCodeTable[AGH_KEYCODE_CAPSLOCK]=	DIK_CAPSLOCK;
+	m_keyCodeTable[AGH_KEYCODE_KANJI]	=	DIK_KANJI;
+	m_keyCodeTable[AGH_KEYCODE_KANA]	=	DIK_KANA;
+	m_keyCodeTable[AGH_KEYCODE_MUHENKAN]=	DIK_NOCONVERT;
+	m_keyCodeTable[AGH_KEYCODE_HENKAN]	=	DIK_CONVERT;
+
+	m_keyCodeTable[AGH_KEYCODE_INSERT]	=	DIK_INSERT;
+	m_keyCodeTable[AGH_KEYCODE_HOME]	=	DIK_HOME;
+	m_keyCodeTable[AGH_KEYCODE_END]		=	DIK_END;
+	m_keyCodeTable[AGH_KEYCODE_PGDOWN]	=	DIK_PGDN;
+	m_keyCodeTable[AGH_KEYCODE_PGUP]	=	DIK_PGUP;
+
+	m_keyCodeTable[AGH_KEYCODE_F1]		=	DIK_F1;
+	m_keyCodeTable[AGH_KEYCODE_F2]		=	DIK_F2;
+	m_keyCodeTable[AGH_KEYCODE_F3]		=	DIK_F3;
+	m_keyCodeTable[AGH_KEYCODE_F4]		=	DIK_F4;
+	m_keyCodeTable[AGH_KEYCODE_F5]		=	DIK_F5;
+	m_keyCodeTable[AGH_KEYCODE_F6]		=	DIK_F6;
+	m_keyCodeTable[AGH_KEYCODE_F7]		=	DIK_F7;
+	m_keyCodeTable[AGH_KEYCODE_F8]		=	DIK_F8;
+	m_keyCodeTable[AGH_KEYCODE_F9]		=	DIK_F9;
+	m_keyCodeTable[AGH_KEYCODE_F10]		=	DIK_F10;
+	m_keyCodeTable[AGH_KEYCODE_F11]		=	DIK_F11;
+	m_keyCodeTable[AGH_KEYCODE_F12]		=	DIK_F12;
+	m_keyCodeTable[AGH_KEYCODE_F13]		=	DIK_F13;
+	m_keyCodeTable[AGH_KEYCODE_F14]		=	DIK_F14;
+	m_keyCodeTable[AGH_KEYCODE_F15]		=	DIK_F15;
+	//m_keyCodeTable[AGH_KEYCODE_F16]	=	DIK_F16;	//	DirectInputでは定義されていないようだ
+
+	m_bKeyCodeTableInited = true;
+}
+
+///////////////////////////////////////////////////////////////////////////
+
 //	コンストラクタ
 CAugustMouseInput::CAugustMouseInput()
 {
+	m_pCore = new CAugustMouseCore();
 }
 
 //	デストラクタ
 CAugustMouseInput::~CAugustMouseInput()
 {
+	delete m_pCore;
 }
-*/
 
-
-//////////////////////////////////////////////////
-
-//	マウス入力
-bool CAugustMouseInput::OnFrameMouseInput()
+void CAugustMouseInput::OnRegist()
 {
-	/*int x = g_->mouse->GetCursorPosX();
-	int y = g_->mouse->GetCursorPosY();*/
-	//POINT point = g_->mouse->GetCursorPos();
-	m_nCachedCurPos = g_->mouse->GetCursorPos();
-	int x = m_nCachedCurPos.x;
-	int y = m_nCachedCurPos.y;
-	m_nCachedCurPosX = x;
-	m_nCachedCurPosY = y;
-	int nMoveX = g_->mouse->GetXMoveCount();
-	int nMoveY = g_->mouse->GetYMoveCount();
+	HWND hWnd = (HWND)MyuAssertNull(GetValPtr(MWLAGH_VALKEY_ROOT_WINDOW_HWND),
+		"CAugustMouseInput::Init()  ウインドウハンドルの取得に失敗");
 
-//#define _P this
-#define _P pControl
-	agh::CVisualControlBase *pControl = GetPosControl(x,y);
-	if ( pControl == NULL )
-		pControl = this;
-
-	/*	例えばドラッグで画面外の時に対応出来ないのでここではチェックしない事とする
-	//	ウインドウの範囲外ならイベント範囲外とする
-	if ( x < 0 || y < 0 ||
-		 x > )
-		return false;
-	*/
-
-	//	割り込み処理
-	OnMouseEventInterrupt(pControl, x, y, nMoveX, nMoveY);
-
-	bool ret = false;
-
-	//	マウス移動
-	if ( nMoveX != 0 || nMoveY != 0 ){
-		//CScreenBase::OnMouseMove(x, y, nMoveX, nMoveY); <- これじゃあ継承した方が呼ばれないぢゃない・・・
-		_P->OnMouseMove(x, y, nMoveX, nMoveY);
-		ret = true;
-	}
-
-	//	真中ボタンを押した
-	if ( g_->mouse->IsOnDownCenterButton() ){
-		//	ウインドウの範囲外ならイベント範囲外とする
-		if ( x < 0 || y < 0 ||
-			 x >= g_->nWindowWidth || y >= g_->nWindowHeight )
-		{
-			//	何もしない。（if文条件反転にすべきだったね...）
-		}
-		else
-		{
-			//CScreenBase::OnCButtonDown(x,y);
-			_P->OnCButtonDown(x,y);
-			ret = true;
-		}
-	}
-
-	//	左ボタンを押した
-	if ( g_->mouse->IsOnDownLeftButton() ){
-
-		//	ウインドウの範囲外ならイベント範囲外とする
-		if ( x < 0 || y < 0 ||
-			 x >= g_->nWindowWidth || y >= g_->nWindowHeight )
-		{
-			//	何もしない。（if文条件反転にすべきだったね...）
-		}
-		else
-		{
-			//CScreenBase::OnLButtonDown(x,y);
-			_P->OnLButtonDown(x,y);
-			ret = true;
-		}
-	}
-
-	//	右ボタンを押した
-	if ( g_->mouse->IsOnDownRightButton() ){
-
-		//	ウインドウの範囲外ならイベント範囲外とする
-		if ( x < 0 || y < 0 ||
-			 x >= g_->nWindowWidth || y >= g_->nWindowHeight )
-		{
-			//	何もしない。（if文条件反転にすべきだったね...）
-		}
-		else
-		{
-			//CScreenBase::OnRButtonDown(x,y);
-			_P->OnRButtonDown(x,y);
-			ret = true;
-		}
-	}
-
-	//// 離した ////////////////////////////
-
-	//	真中ボタンを離した
-	if ( g_->mouse->IsOnUpCenterButton() ){
-		//CScreenBase::OnCButtonUp(x,y);
-		_P->OnCButtonUp(x,y);
-		ret = true;
-	}
-
-	//	左ボタンを離した
-	if ( g_->mouse->IsOnUpLeftButton() ){
-		//CScreenBase::OnLButtonUp(x,y);
-		_P->OnLButtonUp(x,y);
-		ret = true;
-	}
-
-	//	右ボタンを離した
-	if ( g_->mouse->IsOnUpRightButton() ){
-		//CScreenBase::OnRButtonUp(x,y);
-		_P->OnRButtonUp(x,y);
-		ret = true;
-	}
-
-	return ret;
+	((CAugustMouseCore*)m_pCore)->Init(hWnd);
 }
 
-/*
-//	左ボタンが押された
-void CAugustMouseInput::OnLButtonDown(int x, int y)
-{
-	agh::CControlBase *pControl = GetPosControl(x,y);
-	if ( pControl == NULL )
-		pControl->OnLButtonDown(x,y);
-	else
-		OnBackgroundLButtonDown(x,y);
-}
-*/
 
-/*
-//	左ボタンが押された
-void CAugustMouseInput::OnLButtonDown(int x, int y)
-{
-	//	コントロールで該当するものがあるか？
-	for(int i=0; i<m_controlPtrAry.size(); i++)
-	{
-		CControlBase *p = m_controlPtrAry[i];
-		_AGH_RECT& rect = p->GetRect();
-
-		if ( IsRectInternal((_MSL_RECT*)&rect, x, y) ){
-			OnControlLButtonDown(p,x,y);
-			return;
-		}
-	}
-
-	//	どのコントロールにも一致しない場合はバックグラウンド
-	OnBackgroundLButtonDown(x,y);
-}
-*/
--- mgllib/src/august/AugustMouseInput.h
+++ mgllib/src/august/AugustMouseInput.h
@@ -8,176 +8,21 @@
 #define __AugustMouseInput_H__
 
 #include "agh.h"
-
-//////////////////////////////////////////////////////////////////////////////////////
-
-//	キーボードイベントハンドラ系
-
-#define AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_PRESS		(1)	//	押されている間
-#define AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_KEYDOWN	(2)	//	キーボードが押された瞬間
-#define AUGUST_KB_EVT_HANDLER_EVTTYPE_ON_KEYUP		(3)	//	キーボードが離された瞬間
-/*#define MGL_KB_EVT_HANDLER_EVTTYPE_ON_DOWNUP	(8)	//	押した後話した（クリック的な）
-#define MGL_KB_EVT_HANDLER_EVTTYPE_ON_PUSH		(MGL_KB_EVT_HANDLER_EVTTYPE_ON_DOWNUP)*/
-
-//	キーボードイベントハンドラ構造体
-class CAugustMouseInput;
-typedef bool (CAugustMouseInput::*AUGUST_KB_EVT_HANDLER_CALLBACK)();
-typedef struct {
-	AUGUST_KB_EVT_HANDLER_CALLBACK pCallbackFunc;
-	BYTE keyCode;
-	short evtType;
-} AUGUST_KB_EVT_HANDLER;
-typedef list<AUGUST_KB_EVT_HANDLER> t_AUGUST_KB_EVT_HANDLERS;
-
-class CMglMouseInput;
-
-/////////////////////////////////////////////////////////////////////////
+#include "AugustCommon2.h"
 
 //	クラス宣言  /////////////////////////////////////////////////////////
-class DLL_EXP CAugustMouseInput : public agh::CControlBase
+
+class _AGST_DLL_EXP CAugustMouseInput : public agh::CMouseBase
 {
-protected:
-	CMglMouseInput *m_pMouse;
-
-	//list<bool (*)()> m_kbEventHandlers;	//	本当はvector_list使うネ・・・
-	//list<MGL_KB_EVT_HANDLER_CALLBACK> m_kbEventHandlers;	//	本当はvector_list使うネ・・・
-	list<AUGUST_KB_EVT_HANDLER> m_kbEventHandlers;	//	本当はvector_list使うネ・・・
-
-	/////////////////////////////////////////////////////////
-
-	POINT m_nCachedCurPos;
-	int m_nCachedCurPosX;
-	int m_nCachedCurPosY;
-	bool m_bUseMouseHandle;
-
-	agh::_AGH_POINT GetCurPos(){
-		return *((agh::_AGH_POINT*)&m_nCachedCurPos);
-	}
-
+	//	イベントハンドラの実装
 _AGH_EVENT_ACCESS_MODIFIER:
-	///// オーバーライド可能なイベント /////////////////////////////////////////////////
-
-	/**
-	  ここにあった OnControl, OnBackground は下に移動しといたよ。
-	**/
-	virtual bool DoFrame();
-
-protected:
-
-private:
-	//	なんでPublic？（Privateではないのか・・・？）
-	//void OnLButtonDown(int x, int y);
-
-	//void ScreenUpdate();
+	virtual void OnRegist();
 
 public:
-	//	コンストラクタ
-	/*CAugustMouseInput(CAugustGlobalCommon &g_in) : g_(g_in),
-		m_mouse(g_in.input.mouse), m_grp(g_in.grp), m_input(g_in.input), m_audio(g_in.audio),
-		m_imgCache(g_in.imgCache)
-	{
-		//m_hWnd = NULL;
-		m_rgbBackground = D3DCOLOR_WHITE;
-	}*/
-	//	コンストラクタ
-	CAugustMouseInput()
-	{
-		//m_hWnd = NULL;
-		ZeroMemory(&m_nCachedCurPos, sizeof(m_nCachedCurPos));
-		m_nCachedCurPosX = -1;
-		m_nCachedCurPosY = -1;
-		m_bUseMouseHandle = false;
-	}
-	virtual ~CAugustMouseInput(){}
-
-	//////////////////////////////////////////////////////
-
-	//	キーボードハンドラの登録
-	void RegistKbHandler(AUGUST_KB_EVT_HANDLER &evt){
-		m_kbEventHandlers.push_back(evt);
-	}
-	void RegistKbHandler(
-		short evtType,
-		BYTE keyCode,		
-		AUGUST_KB_EVT_HANDLER_CALLBACK pCallbackFunc)
-	{
-		AUGUST_KB_EVT_HANDLER evt;
-		evt.pCallbackFunc = pCallbackFunc;
-		evt.keyCode = keyCode;
-		evt.evtType = evtType;
-		m_kbEventHandlers.push_back(evt);
-	}
-
-	void EnableMouseHandle(){ m_bUseMouseHandle = true; }
-	void DisableMouseHandle(){ m_bUseMouseHandle = false; }
-
-public:
-	//bool OnFrameMouseInput(); <- なんかpublicなのに理由あんのかな・・・？
+	//	コンストラクタ・デストラクタ
+	CAugustMouseInput();
+	virtual ~CAugustMouseInput();
 };
 
-
-
-	/*virtual void OnBackgroundLButtonDown(int x, int y){}
-	virtual void OnBackgroundRButtonDown(int x, int y){}
-	virtual void OnBackgroundCButtonDown(int x, int y){}
-	virtual void OnControlLButtonDown(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnControlRButtonDown(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnControlCButtonDown(agh::CControlBase *pControl, int x, int y){}
-
-	//	左クリック系 : Left Click Events
-	virtual void OnLButtonClick(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnLButtonDblClk(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnLButtonDown(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnLButtonUp(agh::CControlBase *pControl, int x, int y){}
-
-	//	右クリック系 : Right Click Events
-	virtual void OnRButtonClick(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnRButtonDblClk(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnRButtonDown(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnRButtonUp(agh::CControlBase *pControl, int x, int y){}
-
-	//	マウス移動 : Move Mouse Events
-	virtual void OnMouseMove(agh::CControlBase *pControl, int nPosX, int nPosY, int nMoveX, int nMoveY){}
-
-	//	ホイール : Wheel Events
-	virtual void OnWheelUp(agh::CControlBase *pControl, int count){}
-	virtual void OnWheelDown(agh::CControlBase *pControl, int count){}
-	virtual void OnCButtonClick(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnCButtonDblClk(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnCButtonDown(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnCButtonUp(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnMButtonClick(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnMButtonDblClk(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnMButtonDown(agh::CControlBase *pControl, int x, int y){}
-	virtual void OnMButtonUp(agh::CControlBase *pControl, int x, int y){}*/
-
-	/*
-	//	左クリック系 : Left Click Events
-	virtual void OnBackgroundLButtonClick(int x, int y){}
-	virtual void OnBackgroundLButtonDblClk(int x, int y){}
-	virtual void OnBackgroundLButtonDown(int x, int y){}
-	virtual void OnBackgroundLButtonUp(int x, int y){}
-
-	//	右クリック系 : Right Click Events
-	virtual void OnBackgroundRButtonClick(int x, int y){}
-	virtual void OnBackgroundRButtonDblClk(int x, int y){}
-	virtual void OnBackgroundRButtonDown(int x, int y){}
-	virtual void OnBackgroundRButtonUp(int x, int y){}
-
-	//	マウス移動 : Move Mouse Events
-	virtual void OnBackgroundMouseMove(int nPosX, int nPosY, int nMoveX, int nMoveY){}
-
-	//	ホイール : Wheel Events
-	virtual void OnBackgroundWheelUp(int count){}
-	virtual void OnBackgroundWheelDown(int count){}
-	virtual void OnBackgroundCButtonClick(int x, int y){}
-	virtual void OnBackgroundCButtonDblClk(int x, int y){}
-	virtual void OnBackgroundCButtonDown(int x, int y){}
-	virtual void OnBackgroundCButtonUp(int x, int y){}
-	virtual void OnBackgroundMButtonClick(int x, int y){}
-	virtual void OnBackgroundMButtonDblClk(int x, int y){}
-	virtual void OnBackgroundMButtonDown(int x, int y){}
-	virtual void OnBackgroundMButtonUp(int x, int y){}
-	*/
 
 #endif//__AugustMouseInput_H__
