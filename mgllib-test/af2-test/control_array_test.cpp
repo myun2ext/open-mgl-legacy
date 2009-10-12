67138eb3b12abfe44f1fb1b511b478649ea1e241
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-10-12 08:02:04 +0000

    std::vector は push_back() でメモリアドレス変わっちゃうから駄目だって・・・
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@1304 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib-test/af2-test/control_array_test.cpp
+++ mgllib-test/af2-test/control_array_test.cpp
@@ -1,7 +1,7 @@
 #include "mglaugust2.h"
 
 
-
+//	アイテムクラス
 class CFileItem : public CAugustText2
 {
 public:
@@ -14,6 +14,8 @@ _AGH_EVENT_ACCESS_MODIFIER:
 	}
 };
 
+
+//	アレイクラス
 class CFileItemArray : public agh::CControlList<CFileItem>
 {
 private:
@@ -29,33 +31,13 @@ public:
 _AGH_EVENT_ACCESS_MODIFIER:
 };
 
-class CFileItemArray1 : public agh::CVisualControlBase
-{
-protected:
-	std::vector<CFileItem> m_items;
-public:
-	void Add(){
-		m_items.push_back( CFileItem() );
 
-		CFileItem *pAddedItem = m_items.rbegin().operator ->();
-		RegistControl(pAddedItem);
-
-		pAddedItem->SetPos(7, m_items.size()*17);
-	}
-};
-
-
-
-
+//	メインクラス
 //class CMyWindow : public CMwlAghWindow
 class CMyWindow : public CAugustScreen2//, public agh::_CKeyboardBase_CDummyBase
 {
 private:
-	agh::CControlVector<CAugustText2> m_textAry;
-	CFileItemArray1 m_textAry1;
 	CFileItemArray m_itemAry;
-
-	//std::vector::iterator it;
 
 public:
 	//	コンストラクタ・デストラクタ
@@ -64,18 +46,14 @@ public:
 	
 	virtual bool OnGraphicInitEnded()
 	{
-		RegistSubControl(&m_textAry);
-		RegistSubControl(&m_textAry1);
 		RegistSubControl(&m_itemAry);
 
-		CAugustText2 m_text3 = m_text2;
+		//CAugustText2 m_text3 = m_text2;
 
 		//m_textAry.Add().SetText("ZZZZZZZZZZZZZZZZ");
 		//m_textAry.Add().SetText("YYYYYYYYYYYYYY");
-		m_textAry1.Add();
-		m_textAry1.Add();
-		//m_itemAry.Add();
-		//m_itemAry.Add();
+		m_itemAry.Add();
+		m_itemAry.Add();
 
 		return true;
 	}
