ab902f61da310b5ef7648f8f4676f0a549ee1263
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-11-15 10:46:43 +0000

    CMyFrame の作成
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@1464 6d62ff88-fa28-0410-b5a4-834eb811a934

--- mgllib-bench/mgllib-bench/mgllib-bench.cpp
+++ mgllib-bench/mgllib-bench/mgllib-bench.cpp
@@ -1,15 +1,27 @@
 #include "StdAfx.h"
 
-//////////////////////////////////////////////////////
+//	ƒtƒŒ[ƒ€
+class CMyFrame : public CAugustWindowFrame2
+{
+private:
+	CAugustImage2 m_img;
+public:
+	//	‰Šú‰»‚ÉŒÄ‚Î‚ê‚é
+	bool OnReady(){
+		RegistControl(&m_img);
+		m_img.Load("test.jpg");
 
+		return true;
+	}
+};
+
+//	WinMain
 int APIENTRY WinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
                      LPSTR     lpCmdLine,
                      int       nCmdShow )
 {
-	CAugustScreen2 frame;
+	CMyFrame frame;
 	frame.Start();
 	return 0;
 }
-
-
