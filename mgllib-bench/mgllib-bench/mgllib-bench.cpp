0efc96eb77ddf6aec72c00efd50019c448b43717
myun2 <myun2@6d62ff88-fa28-0410-b5a4-834eb811a934>
2009-11-15 09:36:32 +0000

    StdAfx 追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/open-mgl/MGL/trunk@1462 6d62ff88-fa28-0410-b5a4-834eb811a934

---
+++ mgllib-bench/mgllib-bench/mgllib-bench.cpp
@@ -0,0 +1,15 @@
+#include "StdAfx.h"
+
+//////////////////////////////////////////////////////
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow )
+{
+	/*CAugustScreen2 frame;
+	frame.Start();*/
+	return 0;
+}
+
+
